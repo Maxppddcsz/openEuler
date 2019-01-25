0d1bdb32b173ca4a13619c592d53addb2061674f
Xiaofei Tan <tanxiaofei@huawei.com>
2019-01-25 22:22:35 +0800

    scsi: hisi_sas: Fix losing directly attached disk when hot-plug
    mainline inclusion
    from mainline-v5.1-rc1
    commit b6c9b15e44090aee2a7fba646b06ff166f595b16
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8EKNE
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=b6c9b15e44090aee2a7fba646b06ff166f595b16
    
    ----------------------------------------------------------------------
    
    Hot-plugging SAS wire of direct hard disk backplane may cause disk lost. We
    have done this test with several types of SATA disk from different venders,
    and only two models from Seagate has this problem, ST4000NM0035-1V4107 and
    ST3000VM002-1ET166.
    
    The root cause is that the disk doesn't send D2H frame after OOB finished.
    SAS controller will issue phyup interrupt only when D2H frame is received,
    otherwise, will be waiting there all the time.
    
    When this issue happen, we can find the disk again with link reset.  To fix
    this issue, we setup an timer after OOB finished. If the PHY is not up in
    20s, do link reset. Notes: the 20s is an experience value.
    
    Signed-off-by: Xiaofei Tan <tanxiaofei@huawei.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: YunYi Yang <yangyunyi2@huawei.com>
    
     Conflicts:
    	drivers/scsi/hisi_sas/hisi_sas_main.c
    	drivers/scsi/hisi_sas/hisi_sas_v2_hw.c
    	drivers/scsi/hisi_sas/hisi_sas_v3_hw.c

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -608,6 +608,7 @@ extern void hisi_sas_init_mem(struct hisi_hba *hisi_hba);
 extern void hisi_sas_rst_work_handler(struct work_struct *work);
 extern void hisi_sas_sync_rst_work_handler(struct work_struct *work);
 extern void hisi_sas_kill_tasklets(struct hisi_hba *hisi_hba);
+extern void hisi_sas_phy_oob_ready(struct hisi_hba *hisi_hba, int phy_no);
 extern bool hisi_sas_notify_phy_event(struct hisi_sas_phy *phy,
 				enum hisi_sas_phy_event event);
 extern void hisi_sas_release_tasks(struct hisi_hba *hisi_hba);
