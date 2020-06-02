177048e6f16fe71db4da621fd6c57043c6ea434b
Jiri Olsa <jolsa@kernel.org>
2020-06-02 23:47:40 +0200

    perf tests: Add parse metric test for ipc metric
    mainline inclusion
    from mainline-v5.9-rc1
    commit acf71b05d1a19726594a8436ba9d8af871941e6c
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8C0CX
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=acf71b05d1a19726594a8436ba9d8af871941e6c
    
    ----------------------------------------------------------------------
    
    Adding new test that process metrics code and checks the expected
    results. Starting with easy ipc metric.
    
    Committer testing:
    
      # perf test "Parse and process metrics"
      67: Parse and process metrics                             : Ok
      #
      # perf test -v "Parse and process metrics"
      67: Parse and process metrics                             :
      --- start ---
      test child forked, pid 103402
      metric expr inst_retired.any / cpu_clk_unhalted.thread for IPC
      found event inst_retired.any
      found event cpu_clk_unhalted.thread
      adding {inst_retired.any,cpu_clk_unhalted.thread}:W
      test child finished with 0
      ---- end ----
      Parse and process metrics: Ok
      #
    
    Had to fix it to initialize that 'struct value' array sentinel with a
    named initializer to fix the build with some versions of clang:
    
      tests/parse-metric.c:135:7: error: missing field 'val' initializer [-Werror,-Wmissing-field-initializers]
                    { 0 },
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Acked-by: Ian Rogers <irogers@google.com>
    Tested-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Link: http://lore.kernel.org/lkml/20200602214741.1218986-13-jolsa@kernel.org
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: hongrongxuan <hongrongxuan@huawei.com>

--- tools/perf/tests/tests.h
+++ tools/perf/tests/tests.h
@@ -110,6 +110,7 @@ int test__clang_subtest_get_nr(void);
 int test__unit_number__scnprint(struct test *test, int subtest);
 int test__mem2node(struct test *t, int subtest);
 int test__demangle_java(struct test *test, int subtest);
+int test__parse_metric(struct test *test, int subtest);
 
 bool test__bp_signal_is_supported(void);
 
