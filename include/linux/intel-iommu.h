5d637c5f7dfbc58248f31b20fff6813b1b5d3950
Jason Zeng <jason.zeng@intel.com>
2021-01-20 12:44:15 +0800

    intel-iommu: introduce keepalive flag and devinfo list for intel_iommu
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- include/linux/intel-iommu.h
+++ include/linux/intel-iommu.h
@@ -610,6 +610,9 @@ struct intel_iommu {
 	u32		flags;      /* Software defined flags */
 
 	struct dmar_drhd_unit *drhd;
+
+	struct list_head devinfo_list;
+	bool keepalive;
 };
 
 /* Per subdevice private data */
