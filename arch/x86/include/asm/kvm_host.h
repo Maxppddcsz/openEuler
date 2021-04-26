0ea256249e6c989e7f9d0e7f0aac47e4d96a1632
hanliyang <hanliyang@hygon.cn>
2021-04-26 10:47:46 +0800

    KVM: x86: Support VM_ATTESTATION hypercall
    hygon inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I98W95
    CVE: NA
    
    ---------------------------
    
    When sev guest wants to collect the attestation report, it cannot directly
    communicate with psp. Add VM_ATTESTATION hypercall to allow sev guest to
    tell host to help get the attestation report. Since sev guest memory is
    encrypted, host cannot tamper with the report data.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1770,6 +1770,8 @@ struct kvm_x86_ops {
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
 
 	gva_t (*get_untagged_addr)(struct kvm_vcpu *vcpu, gva_t gva, unsigned int flags);
+
+	int (*vm_attestation)(struct kvm *kvm, unsigned long gpa, unsigned long len);
 };
 
 struct kvm_x86_nested_ops {
