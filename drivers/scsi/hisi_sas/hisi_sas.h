eae6b7ab7dbc352bf4cd5c5a7a44c606c49ea982
Luo Jiaxing <luojiaxing@huawei.com>
2021-08-03 14:47:27 +0800

    scsi: hisi_sas: Move debugfs code to v3 hw driver
    mainline inclusion
    from mainline-v5.11-rc1
    commit 623a4b6d5c2a7595f677fa17348dbca6b461f16a
    category: feature
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8F81L
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=623a4b6d5c2a7595f677fa17348dbca6b461f16a
    
    ----------------------------------------------------------------------
    
    Relocate all the debugfs code for DFX to v3 hw since no other versions
    support it.
    
    Link: https://lore.kernel.org/r/1606207594-196362-4-git-send-email-john.garry@huawei.com
    Signed-off-by: Luo Jiaxing <luojiaxing@huawei.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Reviewed-by: Ouyangdelong <ouyangdelong@huawei.com>
    Signed-off-by: Nifujia <nifujia1@hisilicon.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: YunYi Yang <yangyunyi2@huawei.com>
    
     Conflicts:
    	drivers/scsi/hisi_sas/hisi_sas.h
    	drivers/scsi/hisi_sas/hisi_sas_main.c
    	drivers/scsi/hisi_sas/hisi_sas_v3_hw.c

--- drivers/scsi/hisi_sas/hisi_sas.h
+++ drivers/scsi/hisi_sas/hisi_sas.h
@@ -260,24 +260,6 @@ struct hisi_sas_slot {
 	int	idx;
 };
 
-#define HISI_SAS_DEBUGFS_REG(x) {#x, x}
-
-struct hisi_sas_debugfs_reg_lu {
-	char *name;
-	int off;
-};
-
-struct hisi_sas_debugfs_reg {
-	const struct hisi_sas_debugfs_reg_lu *lu;
-	int count;
-	int base_off;
-	union {
-		u32 (*read_global_reg)(struct hisi_hba *hisi_hba, u32 off);
-		u32 (*read_port_reg)(struct hisi_hba *hisi_hba, int port,
-				     u32 off);
-	};
-};
-
 enum {
 	HISI_SAS_BIST_LOOPBACK_MODE_DIGITAL = 0,
 	HISI_SAS_BIST_LOOPBACK_MODE_SERDES,
@@ -377,15 +359,8 @@ struct hisi_sas_hw {
 	const struct cpumask *(*get_managed_irq_aff)(struct hisi_hba
 			*hisi_hba, int queue);
 	void (*debugfs_work_handler)(struct work_struct *work);
-	void (*read_iost_itct_cache)(struct hisi_hba *hisi_hba,
-				     enum hisi_sas_debugfs_cache_type type,
-				     u32 *cache);
 	int complete_hdr_size;
 	struct scsi_host_template *sht;
-
-	const struct hisi_sas_debugfs_reg *debugfs_reg_array[DEBUGFS_REGS_NUM];
-	const struct hisi_sas_debugfs_reg *debugfs_reg_port;
-	int (*set_bist)(struct hisi_hba *hisi_hba, bool enable);
 };
 
 #define HISI_SAS_MAX_DEBUGFS_DUMP (50)
@@ -710,8 +685,5 @@ extern void hisi_sas_release_tasks(struct hisi_hba *hisi_hba);
 extern u8 hisi_sas_get_prog_phy_linkrate_mask(enum sas_linkrate max);
 extern void hisi_sas_controller_reset_prepare(struct hisi_hba *hisi_hba);
 extern void hisi_sas_controller_reset_done(struct hisi_hba *hisi_hba);
-extern void hisi_sas_debugfs_init(struct hisi_hba *hisi_hba);
-extern void hisi_sas_debugfs_exit(struct hisi_hba *hisi_hba);
 extern void hisi_sas_snapshot_regs(struct hisi_hba *hisi_hba);
-extern void hisi_sas_debugfs_work_handler(struct work_struct *work);
 #endif
