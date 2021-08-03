3543effc59c200d24b33b8793ef27c37f4379158
Ahmed S. Darwish <a.darwish@linutronix.de>
2021-08-03 14:47:39 +0800

    scsi: hisi_sas: Pass gfp_t flags to libsas event notifiers
    mainline inclusion
    from mainline-v5.12-rc1
    commit 26c7efc3f95260fd90e6cb268b47a58cf27ffc64
    category: feature
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8EKNE
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=26c7efc3f95260fd90e6cb268b47a58cf27ffc64
    
    ----------------------------------------------------------------------
    
    Use the new libsas event notifiers API, which requires callers to
    explicitly pass the gfp_t memory allocation flags.
    
    Below are the context analysis for modified functions:
    
    => hisi_sas_bytes_dmaed():
    
    Since it is invoked from both process and atomic contexts, let its callers
    pass the gfp_t flags:
    
      * hisi_sas_main.c:
      ------------------
    
        hisi_sas_phyup_work(): workqueue context
          -> hisi_sas_bytes_dmaed(..., GFP_KERNEL)
    
        hisi_sas_controller_reset_done(): has an msleep()
          -> hisi_sas_rescan_topology()
            -> hisi_sas_phy_down()
              -> hisi_sas_bytes_dmaed(..., GFP_KERNEL)
    
        hisi_sas_debug_I_T_nexus_reset(): calls wait_for_completion_timeout()
          -> hisi_sas_phy_down()
            -> hisi_sas_bytes_dmaed(..., GFP_KERNEL)
    
      * hisi_sas_v1_hw.c:
      -------------------
    
        int_abnormal_v1_hw(): irq handler
          -> hisi_sas_phy_down()
            -> hisi_sas_bytes_dmaed(..., GFP_ATOMIC)
    
      * hisi_sas_v[23]_hw.c:
      ----------------------
    
        int_phy_updown_v[23]_hw(): irq handler
          -> phy_down_v[23]_hw()
            -> hisi_sas_phy_down()
              -> hisi_sas_bytes_dmaed(..., GFP_ATOMIC)
    
    => int_bcast_v1_hw() and phy_bcast_v3_hw():
    
    Both are invoked exclusively from irq handlers. Pass GFP_ATOMIC.
    
    Link: https://lore.kernel.org/r/20210118100955.1761652-12-a.darwish@linutronix.de
    Reviewed-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Ahmed S. Darwish <a.darwish@linutronix.de>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Reviewed-by: Ouyangdelong <ouyangdelong@huawei.com>
    Signed-off-by: Nifujia <nifujia1@hisilicon.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: YunYi Yang <yangyunyi2@huawei.com>
    
     Conflicts:
    	drivers/scsi/hisi_sas/hisi_sas_main.c
    	drivers/scsi/hisi_sas/hisi_sas_v1_hw.c
    	drivers/scsi/hisi_sas/hisi_sas_v2_hw.c
    	drivers/scsi/hisi_sas/hisi_sas_v3_hw.c

--- drivers/scsi/hisi_sas/hisi_sas_main.c
+++ drivers/scsi/hisi_sas/hisi_sas_main.c
@@ -628,17 +628,16 @@ static int hisi_sas_task_exec(struct sas_task *task, gfp_t gfp_flags,
 	return rc;
 }
 
-static void hisi_sas_bytes_dmaed(struct hisi_hba *hisi_hba, int phy_no)
+static void hisi_sas_bytes_dmaed(struct hisi_hba *hisi_hba, int phy_no,
+				 gfp_t gfp_flags)
 {
 	struct hisi_sas_phy *phy = &hisi_hba->phy[phy_no];
 	struct asd_sas_phy *sas_phy = &phy->sas_phy;
-	struct sas_ha_struct *sas_ha;
 
 	if (!phy->phy_attached)
 		return;
 
-	sas_ha = &hisi_hba->sha;
-	sas_ha->notify_phy_event(sas_phy, PHYE_OOB_DONE);
+	sas_notify_phy_event_gfp(sas_phy, PHYE_OOB_DONE, gfp_flags);
 
 	if (sas_phy->phy) {
 		struct sas_phy *sphy = sas_phy->phy;
@@ -666,7 +665,7 @@ static void hisi_sas_bytes_dmaed(struct hisi_hba *hisi_hba, int phy_no)
 	}
 
 	sas_phy->frame_rcvd_size = phy->frame_rcvd_size;
-	sas_ha->notify_port_event(sas_phy, PORTE_BYTES_DMAED);
+	sas_notify_port_event_gfp(sas_phy, PORTE_BYTES_DMAED, gfp_flags);
 }
 
 static struct hisi_sas_device *hisi_sas_alloc_dev(struct domain_device *device)
@@ -881,7 +880,7 @@ static void hisi_sas_phyup_work(struct work_struct *work)
 
 	if (phy->identify.target_port_protocols == SAS_PROTOCOL_SSP)
 		hisi_hba->hw->sl_notify_ssp(hisi_hba, phy_no);
-	hisi_sas_bytes_dmaed(hisi_hba, phy_no);
+	hisi_sas_bytes_dmaed(hisi_hba, phy_no, GFP_KERNEL);
 }
 
 static void hisi_sas_linkreset_work(struct work_struct *work)
@@ -1452,7 +1451,6 @@ static void hisi_sas_refresh_port_id(struct hisi_hba *hisi_hba)
 
 static void hisi_sas_rescan_topology(struct hisi_hba *hisi_hba, u32 state)
 {
-	struct sas_ha_struct *sas_ha = &hisi_hba->sha;
 	struct asd_sas_port *_sas_port = NULL;
 	int phy_no;
 
@@ -1473,11 +1471,12 @@ static void hisi_sas_rescan_topology(struct hisi_hba *hisi_hba, u32 state)
 				_sas_port = sas_port;
 
 				if (DEV_IS_EXPANDER(dev->dev_type))
-					sas_ha->notify_port_event(sas_phy,
-							PORTE_BROADCAST_RCVD);
+					sas_notify_port_event_gfp(sas_phy,
+							PORTE_BROADCAST_RCVD,
+							GFP_KERNEL);
 			}
 		} else {
-			hisi_sas_phy_down(hisi_hba, phy_no, 0);
+			hisi_sas_phy_down(hisi_hba, phy_no, 0, GFP_KERNEL);
 		}
 	}
 }
@@ -1858,7 +1857,7 @@ static int hisi_sas_debug_I_T_nexus_reset(struct domain_device *device)
 		/* report PHY down if timed out */
 		if (!ret) {
 			dev_warn(dev, "phy%d reset timeout\n", sas_phy->id);
-			hisi_sas_phy_down(hisi_hba, sas_phy->id, 0);
+			hisi_sas_phy_down(hisi_hba, sas_phy->id, 0, GFP_KERNEL);
 		}
 	} else if (sas_dev->dev_status != HISI_SAS_DEV_INIT)
 		/* Sleep 2s, wait for I_T reset at expander env except fail */
@@ -2268,16 +2267,16 @@ static void hisi_sas_phy_disconnected(struct hisi_sas_phy *phy)
 	spin_unlock_irqrestore(&phy->lock, flags);
 }
 
-void hisi_sas_phy_down(struct hisi_hba *hisi_hba, int phy_no, int rdy)
+void hisi_sas_phy_down(struct hisi_hba *hisi_hba, int phy_no, int rdy,
+		       gfp_t gfp_flags)
 {
 	struct hisi_sas_phy *phy = &hisi_hba->phy[phy_no];
 	struct asd_sas_phy *sas_phy = &phy->sas_phy;
-	struct sas_ha_struct *sas_ha = &hisi_hba->sha;
 	struct device *dev = hisi_hba->dev;
 
 	if (rdy) {
 		/* Phy down but ready */
-		hisi_sas_bytes_dmaed(hisi_hba, phy_no);
+		hisi_sas_bytes_dmaed(hisi_hba, phy_no, gfp_flags);
 		hisi_sas_port_notify_formed(sas_phy);
 	} else {
 		struct hisi_sas_port *port  = phy->port;
@@ -2288,7 +2287,8 @@ void hisi_sas_phy_down(struct hisi_hba *hisi_hba, int phy_no, int rdy)
 			return;
 		}
 		/* Phy down and not ready */
-		sas_ha->notify_phy_event(sas_phy, PHYE_LOSS_OF_SIGNAL);
+		sas_notify_phy_event_gfp(sas_phy,
+					 PHYE_LOSS_OF_SIGNAL, gfp_flags);
 		sas_phy_disconnected(sas_phy);
 
 		if (port) {
