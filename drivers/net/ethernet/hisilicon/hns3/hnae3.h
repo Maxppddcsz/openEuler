b34e9d12e32cf0596a3afed3b566db6d39f83534
Guangbin Huang <huangguangbin2@huawei.com>
2021-11-18 20:44:42 +0800

    net: hns3: device specifications add number of mac statistics
    mainline inclusion
    from mainline-v5.16-rc1
    commit 4e4c03f6ab636e9c39558845da5bfbcd60baf33d
    category: feature
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8EMYT
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=4e4c03f6ab636e9c39558845da5bfbcd60baf33d
    
    ----------------------------------------------------------------------
    
    Currently, driver queries number of mac statistics before querying mac
    statistics. As the number of mac statistics is a fixed value in firmware,
    it is redundant to query this number everytime before querying mac
    statistics, it can just be queried once in initialization process and
    saved in device specifications.
    
    Signed-off-by: Guangbin Huang <huangguangbin2@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Reviewed-by: Yongxin Li <liyongxin1@huawei.com>
    Signed-off-by: Junxin Chen <chenjunxin1@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Xiaodong Li <lixiaodong67@huawei.com>
    
     Conflicts:
    	drivers/net/ethernet/hisilicon/hns3/hnae3.h
    	drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c

--- drivers/net/ethernet/hisilicon/hns3/hnae3.h
+++ drivers/net/ethernet/hisilicon/hns3/hnae3.h
@@ -349,6 +349,9 @@ struct hnae3_dev_specs {
 	u16 max_int_gl; /* max value of interrupt coalesce based on INT_GL */
 	u8 max_non_tso_bd_num; /* max BD number of one non-TSO packet */
 	u16 max_qset_num;
+	u16 umv_size;
+	u16 mc_mac_size;
+	u32 mac_stats_num;
 };
 
 struct hnae3_client_ops {
--- drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3_debugfs.c
@@ -780,6 +780,8 @@ hns3_dbg_dev_specs(struct hnae3_handle *h, char *buf, int len, int *pos)
 			  dev_specs->max_tm_rate);
 	*pos += scnprintf(buf + *pos, len - *pos, "MAX QSET number: %u\n",
 			  dev_specs->max_qset_num);
+	*pos += scnprintf(buf + *pos, len - *pos, "MAC statistics number: %u\n",
+			  dev_specs->mac_stats_num);
 }
 
 static int hns3_dbg_dev_info(struct hnae3_handle *h, char *buf, int len)
