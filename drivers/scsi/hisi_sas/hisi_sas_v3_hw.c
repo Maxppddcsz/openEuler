084f79579d62df60fd88e6f9bb5a7231990f344b
Xiang Chen <chenxiang66@hisilicon.com>
2022-04-19 17:07:00 +0800

    scsi: hisi_sas: Add more logs for runtime suspend/resume
    mainline inclusion
    from mainline-v5.17-rc1
    commit 97f4100939844a6381ba61b99d6d2b1f2fccb79f
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8F803
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=97f4100939844a6381ba61b99d6d2b1f2fccb79f
    
    ----------------------------------------------------------------------
    
    Add some logs at the beginning and end of suspend/resume.
    
    Link: https://lore.kernel.org/r/1639999298-244569-9-git-send-email-chenxiang66@hisilicon.com
    Acked-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Fujai Ni<nifuijia1@hisilicon.com>
    Reviewed-by: Jason Yan <yanaijie@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: YunYi Yang <yangyunyi2@huawei.com>
    
     Conflicts:
    	drivers/scsi/hisi_sas/hisi_sas_v3_hw.c

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -5310,6 +5310,8 @@ static int _suspend_v3_hw(struct device *device)
 	if (test_and_set_bit(HISI_SAS_RESET_BIT, &hisi_hba->flags))
 		return -EPERM;
 
+	dev_warn(dev, "entering suspend state\n");
+
 	scsi_block_requests(shost);
 	set_bit(HISI_SAS_REJECT_CMD_BIT, &hisi_hba->flags);
 	flush_workqueue(hisi_hba->wq);
@@ -5335,6 +5337,8 @@ static int _suspend_v3_hw(struct device *device)
 	hisi_sas_release_tasks(hisi_hba);
 
 	sas_suspend_ha(sha);
+
+	dev_warn(dev, "end of suspending controller\n");
 	return 0;
 }
 
@@ -5381,6 +5385,8 @@ static int _resume_v3_hw(struct device *device)
 	 */
 	sas_resume_ha_no_sync(sha);
 	clear_bit(HISI_SAS_RESET_BIT, &hisi_hba->flags);
+
+	dev_warn(dev, "end of resuming controller\n");
 
 	return 0;
 }
