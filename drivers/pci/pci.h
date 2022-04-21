93fc1869d67cb22e8e64981e92b18d6fdaed6824
Sean V Kelley <sean.v.kelley@intel.com>
2022-04-21 21:49:51 +0800

    PCI/ERR: Add pcie_link_rcec() to associate RCiEPs
    mainline inclusion
    from mainline-v5.11-rc1
    commit 507b460f814458605c47b0ed03c11e49a712fc08
    category: feature
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8EAHA
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=507b460f814458605c47b0ed03c11e49a712fc08
    
    ----------------------------------------------------------------------------
    
    A Root Complex Event Collector terminates error and PME messages from
    associated RCiEPs.
    
    Use the RCEC Endpoint Association Extended Capability to identify
    associated RCiEPs. Link the associated RCiEPs as the RCECs are enumerated.
    
    Co-developed-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Link: https://lore.kernel.org/r/20201121001036.8560-12-sean.v.kelley@intel.com
    Tested-by: Jonathan Cameron <Jonathan.Cameron@huawei.com> # non-native/no RCEC
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>
    Signed-off-by: Sean V Kelley <sean.v.kelley@intel.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Jiefeng Ou <oujiefeng@h-partners.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Jay Fang <f.fangjian@huawei.com>
    Reviewed-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: YunYi Yang <yangyunyi2@huawei.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -447,9 +447,11 @@ static inline void pci_restore_dpc_state(struct pci_dev *dev) {}
 #ifdef CONFIG_PCIEPORTBUS
 void pci_rcec_init(struct pci_dev *dev);
 void pci_rcec_exit(struct pci_dev *dev);
+void pcie_link_rcec(struct pci_dev *rcec);
 #else
 static inline void pci_rcec_init(struct pci_dev *dev) {}
 static inline void pci_rcec_exit(struct pci_dev *dev) {}
+static inline void pcie_link_rcec(struct pci_dev *rcec) {}
 #endif
 
 #ifdef CONFIG_PCI_ATS
