openeuler-mirror/kernel

commit 939325bbad27dc99b8f8cea2791192e5f81b76de

filemap: Correct the conditions for marking a folio as accessed
mainline inclusion
from mainline-5.19-rc4
commit 5ccc944dce3df5fd2fd683a7df4fd49d1068eba2
category: bugfix
bugzilla: 186896, https://gitee.com/src-openeuler/kernel/issues/I5GZC8
CVE: NA

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=5ccc944dce3df5fd2fd683a7df4fd49d1068eba2

-------------------------------------------------

We had an off-by-one error which meant that we never marked the first page
in a read as accessed.  This was visible as a slowdown when re-reading
a file as pages were being evicted from cache too soon.  In reviewing
this code, we noticed a second bug where a multi-page folio would be
marked as accessed multiple times when doing reads that were less than
the size of the folio.

Abstract the comparison of whether two file positions are in the same
folio into a new function, fixing both of these bugs.

Reported-by: Yu Kuai <yukuai3@huawei.com>
Reviewed-by: Kent Overstreet <kent.overstreet@gmail.com>
Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

Conflict: folios is not supported yet
Signed-off-by: Yu Kuai <yukuai3@huawei.com>
Reviewed-by: Kefeng Wang <wangkefeng.wang@huawei.com>
Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

mm/filemap.c
@@ -2437,6 +2437,13 @@ got_pages:
 	goto find_page;
 }
 
+static inline bool pos_same_page(loff_t pos1, loff_t pos2, struct page *page)
+{
+	unsigned int shift = page_shift(page);
+
+	return (pos1 >> shift == pos2 >> shift);
+}
+
 /**
  * generic_file_buffered_read - generic file read routine
  * @iocb:	the iocb to read
@@ -2527,11 +2534,10 @@ ssize_t generic_file_buffered_read(struct kiocb *iocb,
 		writably_mapped = mapping_writably_mapped(mapping);
 
 		/*
-		 * When a sequential read accesses a page several times, only
+		 * When a read accesses a page several times, only
 		 * mark it as accessed the first time.
 		 */
-		if (iocb->ki_pos >> PAGE_SHIFT !=
-		    ra->prev_pos >> PAGE_SHIFT)
+		if (pos_same_page(iocb->ki_pos, ra->prev_pos -1, pages[0]))
 			mark_page_accessed(pages[0]);
 		for (i = 1; i < pg_nr; i++)
 			mark_page_accessed(pages[i]);

Maxppddcsz/openEuler

commit e406ee97eb4f4e0820c881e514fa48a7a28a1d1c

filemap: fix backporting error
uniontech inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I6YDHU
CVE: NA

--------------------------------

There was an error when backporting commit
'5ccc944dce3df5f' from mainline, it should
be '!pos_same_page', but it is 'pos_same_page'
in our code.

Fixes: 939325bbad27dc ("filemap: Correct the conditions for marking a folio as accessed")
Signed-off-by: Gou Hao <gouhao@uniontech.com>

mm/filemap.c
@@ -2630,7 +2630,7 @@ ssize_t generic_file_buffered_read(struct kiocb *iocb,
 		 * When a read accesses a page several times, only
 		 * mark it as accessed the first time.
 		 */
-		if (pos_same_page(iocb->ki_pos, ra->prev_pos -1, pages[0]))
+		if (!pos_same_page(iocb->ki_pos, ra->prev_pos -1, pages[0]))
 			mark_page_accessed(pages[0]);
 		for (i = 1; i < pg_nr; i++)
 			mark_page_accessed(pages[i]);