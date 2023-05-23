78c51d901cdb2e3ac007094e6ef426138b42bd38
Jie Wang <wangjie125@huawei.com>
2023-05-23 10:35:55 +0800

    net: hns3: fix GRE checksum offload issue
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I8EN49
    CVE: NA
    
    ----------------------------------------------------------------------
    
    The device_version V3 hardware can't offload the checksum for IP in GRE
    packets, but can do it for NvGRE. So default to disable the checksum and
    GSO offload for GRE, but keep the ability to enable it when only using
    NvGRE.
    
    Fixes: 76ad4f0ee747 ("net: hns3: Add support of HNS3 Ethernet Driver for hip08 SoC")
    Signed-off-by: Jie Wang <wangjie125@huawei.com>
    (cherry picked from commit df924f69199ae2297033cbe69ac656d7c1aadab6)
    Signed-off-by: Xiaodong Li <lixiaodong67@huawei.com>
    
     Conflicts:
    	drivers/net/ethernet/hisilicon/hns3/hns3_enet.c

--- drivers/net/ethernet/hisilicon/hns3/hnae3.h
+++ drivers/net/ethernet/hisilicon/hns3/hnae3.h
@@ -111,6 +111,9 @@ enum HNAE3_DEV_CAP_BITS {
 #define hnae3_ae_dev_fd_supported(ae_dev) \
 		test_bit(HNAE3_DEV_SUPPORT_FD_B, (ae_dev)->caps)
 
+#define hnae3_ae_dev_gro_supported(ae_dev) \
+		test_bit(HNAE3_DEV_SUPPORT_GRO_B, (ae_dev)->caps)
+
 #define hnae3_dev_fd_supported(hdev) \
 	test_bit(HNAE3_DEV_SUPPORT_FD_B, (hdev)->ae_dev->caps)
 
