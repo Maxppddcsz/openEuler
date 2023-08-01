openeuler-mirror/kernel

commit bc9e3f9895ef257b76601291d99c87c13c7c31df

arm64/mpam: Fix mpam corrupt when cpu online
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I3YAI3
CVE: NA

-------------------------------------------------

The following error occurred occasionally on a machine that supports MPAM:

[   13.321386][  T658] Unable to handle kernel paging request at virtual address ffff80001115816c
[   13.326013][  T684] hid-generic 0003:12D1:0003.0002: input,hidraw1: USB HID v1.10 Mouse [Keyboard/Mouse KVM 1.1.0] on usb-0000:7a:01.0-1.1/input1
[   13.340558][  T658] Mem abort info:
[   13.340563][  T658]   ESR = 0x86000007
[   13.352567][    T5] hub 6-1:1.0: USB hub found
[   13.364750][  T658]   EC = 0x21: IABT (current EL), IL = 32 bits
[   13.369891][    T5] hub 6-1:1.0: 4 ports detected
[   13.373871][  T658]   SET = 0, FnV = 0
[   13.396107][  T658]   EA = 0, S1PTW = 0
[   13.400599][  T658] swapper pgtable: 64k pages, 48-bit VAs, pgdp=0000000029540000
[   13.408726][  T658] [ffff80001115816c] pgd=0000205fffff0003, p4d=0000205fffff0003, pud=0000205fffff0003, pmd=0000205ffffe0003, pte=0000000000000000
[   13.423346][  T658] Internal error: Oops: 86000007 [#1] SMP
[   13.429720][  T658] Modules linked in:
[   13.434243][  T658] CPU: 72 PID: 658 Comm: kworker/72:1 Not tainted 5.10.0-4.17.0.28.oe1.aarch64 #1
[   13.443966][  T658] Hardware name: Huawei TaiShan 200 (Model 2280)/BC82AMDDA, BIOS 1.70 01/07/2021
[   13.453683][  T658] Workqueue: events mpam_enable
[   13.459206][  T658] pstate: 20c00009 (nzCv daif +PAN +UAO -TCO BTYPE=--)
[   13.466625][  T658] pc : mpam_enable+0x194/0x1d8
[   13.472019][  T658] lr : mpam_enable+0x194/0x1d8
[   13.477301][  T658] sp : ffff80004664fd70
[   13.481937][  T658] x29: ffff80004664fd70 x28: 0000000000000000
[   13.488578][  T658] x27: ffff00400484a648 x26: ffff800011b71080
[   13.495306][  T658] x25: 0000000000000000 x24: ffff800011b6cda0
[   13.502001][  T658] x23: ffff800011646f18 x22: ffff800011b6cd80

arch/arm64/kernel/mpam/mpam_device.c
@@ -593,9 +593,11 @@ static void __init mpam_enable(struct work_struct *work)
 		pr_err("Failed to setup/init resctrl\n");
 	mutex_unlock(&mpam_devices_lock);
 
+	local_irq_disable();
 	mpam_cpuhp_state = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
 						"mpam:online", mpam_cpu_online,
 						mpam_cpu_offline);
+	local_irq_enable();
 	if (mpam_cpuhp_state <= 0)
 		pr_err("Failed to re-register 'dyn' cpuhp callbacks");
 	mutex_unlock(&mpam_cpuhp_lock);

Maxppddcsz/openEuler

commit 5037eafe9aaa0dcc08949794f055d5bb948e8610

arm64/mpam: avoid hot-plug mpam_cpu_online() twice in mpam_enable()
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I7PN0A
CVE: NA

-------------------------------------------------

BUG reported when setuping MPAM driver:

[Thu Jul 27 12:15:54 2023] BUG: sleeping function called from invalid context at include/linux/percpu-rwsem.h:49
[Thu Jul 27 12:15:54 2023] in_atomic(): 0, irqs_disabled(): 128, non_block: 0, pid: 593, name: kworker/72:1
[Thu Jul 27 12:15:54 2023] CPU: 72 PID: 593 Comm: kworker/72:1 Not tainted 5.10.0-03467-g02e1abb0f821-dirty #1
[Thu Jul 27 12:15:54 2023] Hardware name: Huawei TaiShan 2280 V2/BC82AMDDA, BIOS 1.79 08/21/2021
[Thu Jul 27 12:15:54 2023] Loaded X.509 cert 'Build time autogenerated kernel key: 9ae2dc86231d0b23cd114b5ed4089cac17566b0f'
[Thu Jul 27 12:15:54 2023] Workqueue: events mpam_enable
[Thu Jul 27 12:15:54 2023] Load PGP public keys
[Thu Jul 27 12:15:54 2023] Call trace:
[Thu Jul 27 12:15:54 2023] dump_backtrace+0x0/0x30c
[Thu Jul 27 12:15:54 2023] show_stack+0x20/0x30
[Thu Jul 27 12:15:54 2023] dump_stack+0x11c/0x174
[Thu Jul 27 12:15:54 2023] ___might_sleep+0x15c/0x1a0
[Thu Jul 27 12:15:54 2023] __might_sleep+0x7c/0x100
[Thu Jul 27 12:15:54 2023] cpus_read_lock+0x3c/0x110
[Thu Jul 27 12:15:54 2023] __cpuhp_setup_state+0x3c/0x80
[Thu Jul 27 12:15:54 2023] mpam_enable+0x148/0x3a4
[Thu Jul 27 12:15:54 2023] process_one_work+0x3cc/0x984
[Thu Jul 27 12:15:54 2023] worker_thread+0x2b0/0x71c
[Thu Jul 27 12:15:54 2023] kthread+0x1e0/0x220
[Thu Jul 27 12:15:54 2023] ret_from_fork+0x10/0x18
[Thu Jul 27 12:15:54 2023] kmemleak: Kernel memory leak detector initialized (mem pool available: 11650)
[Thu Jul 27 12:15:54 2023] kmemleak: Automatic memory scanning thread started
[Thu Jul 27 12:15:54 2023] cryptd: max_cpu_qlen set to 1000
[Thu Jul 27 12:15:54 2023] Key type encrypted registered
[Thu Jul 27 12:15:54 2023] AppArmor: AppArmor sha1 policy hashing enabled
[Thu Jul 27 12:15:54 2023] integrity: Loading X.509 certificate: UEFI:db

Patch: bc9e3f9895ef2 ("arm64/mpam: Fix mpam corrupt when cpu online")
disable irqs when hot-plug mpam_cpu_online() callback twice in
occasional situation re-enter mpam_enable() function concurrently after
probing all mpam devices success, this is not safe as cpuhp_setup_state()
calls sleeping function, we can use lockless atomic_fetch_inc() function
to avoid hot-plug mpam_cpu_online() twice in mpam_enable() instead.

Fixes: bc9e3f9895ef2 ("arm64/mpam: Fix mpam corrupt when cpu online")

arch/arm64/kernel/mpam/mpam_device.c
@@ -540,6 +540,7 @@ static void mpam_disable_irqs(void)
 static void mpam_enable(struct work_struct *work)
 {
 	int err;
+	static atomic_t once;
 	unsigned long flags;
 	struct mpam_device *dev;
 	bool all_devices_probed = true;
@@ -557,7 +558,7 @@ static void mpam_enable(struct work_struct *work)
 	}
 	mutex_unlock(&mpam_devices_lock);
 
-	if (!all_devices_probed)
+	if (!all_devices_probed || atomic_fetch_inc(&once))
 		return;
 
 	mutex_lock(&mpam_devices_lock);
@@ -596,11 +597,9 @@ static void mpam_enable(struct work_struct *work)
 		pr_err("Failed to setup/init resctrl\n");
 	mutex_unlock(&mpam_devices_lock);
 
-	local_irq_disable();
 	mpam_cpuhp_state = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN,
 						"mpam:online", mpam_cpu_online,
 						mpam_cpu_offline);
-	local_irq_enable();
 	if (mpam_cpuhp_state <= 0)
 		pr_err("Failed to re-register 'dyn' cpuhp callbacks");
 	mutex_unlock(&mpam_cpuhp_lock);