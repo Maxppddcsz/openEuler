openeuler-mirror/kernel

commit 7e3625b1eb12c5a4bbc0e8df47f18b1696a4c196

sched: Throttle qos cfs_rq when current cpu is running online task
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I3ZX4D
CVE: NA

--------------------------------

In a co-location scenario, we usually deploy online and
offline task groups in the same server.

The online tasks are more important than offline tasks and to
avoid offline tasks affects online tasks, we will throttle the
offline tasks group when some online task groups running in the
same cpu and unthrottle offline tasks when the cpu is about to
enter idle state.

Signed-off-by: Zhang Qiao <zhangqiao22@huawei.com>
Signed-off-by: Zheng Zucheng <zhengzucheng@huawei.com>
Reviewed-by: Chen Hui <judy.chenhui@huawei.com>
Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

kernel/sched/fair.c
@@ -115,6 +115,10 @@ int __weak arch_asym_cpu_priority(int cpu)
 
 #endif
 
+#ifdef CONFIG_QOS_SCHED
+static DEFINE_PER_CPU_SHARED_ALIGNED(struct list_head, qos_throttled_cfs_rq);
+#endif
+
 #ifdef CONFIG_CFS_BANDWIDTH
 /*
  * Amount of runtime to allocate from global (tg) to local (per-cfs_rq) pool
@@ -7034,6 +7038,124 @@ preempt:
 		set_last_buddy(se);
 }
 
+#ifdef CONFIG_QOS_SCHED
+static void throttle_qos_cfs_rq(struct cfs_rq *cfs_rq)
+{
+	struct rq *rq = rq_of(cfs_rq);
+	struct sched_entity *se;
+	long task_delta, idle_task_delta, dequeue = 1;
+
+	se = cfs_rq->tg->se[cpu_of(rq_of(cfs_rq))];
+
+	/* freeze hierarchy runnable averages while throttled */
+	rcu_read_lock();
+	walk_tg_tree_from(cfs_rq->tg, tg_throttle_down, tg_nop, (void *)rq);
+	rcu_read_unlock();
+
+	task_delta = cfs_rq->h_nr_running;
+	idle_task_delta = cfs_rq->idle_h_nr_running;
+	for_each_sched_entity(se) {
+		struct cfs_rq *qcfs_rq = cfs_rq_of(se);
+		/* throttled entity or throttle-on-deactivate */
+		if (!se->on_rq)
+			break;
+
+		if (dequeue)
+			dequeue_entity(qcfs_rq, se, DEQUEUE_SLEEP);
+		qcfs_rq->h_nr_running -= task_delta;
+		qcfs_rq->idle_h_nr_running -= idle_task_delta;
+
+		if (qcfs_rq->load.weight)
+			dequeue = 0;
+	}
+
+	if (!se)
+		sub_nr_running(rq, task_delta);
+
+	cfs_rq->throttled = 1;
+	cfs_rq->throttled_clock = rq_clock(rq);
+
+	list_add(&cfs_rq->throttled_list, &per_cpu(qos_throttled_cfs_rq, cpu_of(rq)));
+}
+
+static void unthrottle_qos_cfs_rq(struct cfs_rq *cfs_rq)
+{
+	struct rq *rq = rq_of(cfs_rq);
+	struct cfs_bandwidth *cfs_b = tg_cfs_bandwidth(cfs_rq->tg);
+	struct sched_entity *se;
+	int enqueue = 1;
+	long task_delta, idle_task_delta;
+
+	se = cfs_rq->tg->se[cpu_of(rq)];
+
+	cfs_rq->throttled = 0;
+
+	update_rq_clock(rq);
+
+	cfs_b->throttled_time += rq_clock(rq) - cfs_rq->throttled_clock;
+	list_del_init(&cfs_rq->throttled_list);
+
+	/* update hierarchical throttle state */
+	walk_tg_tree_from(cfs_rq->tg, tg_nop, tg_unthrottle_up, (void *)rq);
+
+	if (!cfs_rq->load.weight)
+		return;
+
+	task_delta = cfs_rq->h_nr_running;
+	idle_task_delta = cfs_rq->idle_h_nr_running;
+	for_each_sched_entity(se) {
+		if (se->on_rq)
+			enqueue = 0;
+
+		cfs_rq = cfs_rq_of(se);
+		if (enqueue)
+			enqueue_entity(cfs_rq, se, ENQUEUE_WAKEUP);
+		cfs_rq->h_nr_running += task_delta;
+		cfs_rq->idle_h_nr_running += idle_task_delta;
+
+		if (cfs_rq_throttled(cfs_rq))
+			break;
+	}
+
+	assert_list_leaf_cfs_rq(rq);
+
+	if (!se)
+		add_nr_running(rq, task_delta);
+
+	/* Determine whether we need to wake up potentially idle CPU: */
+	if (rq->curr == rq->idle && rq->cfs.nr_running)
+		resched_curr(rq);
+}
+
+static int unthrottle_qos_cfs_rqs(int cpu)
+{
+	struct cfs_rq *cfs_rq, *tmp_rq;
+	int res = 0;
+
+	list_for_each_entry_safe(cfs_rq, tmp_rq, &per_cpu(qos_throttled_cfs_rq, cpu),
+				throttled_list) {
+		if (cfs_rq_throttled(cfs_rq)) {
+			unthrottle_qos_cfs_rq(cfs_rq);
+			res++;
+		}
+	}
+
+	return res;
+}
+
+static bool check_qos_cfs_rq(struct cfs_rq *cfs_rq)
+{
+	if (unlikely(cfs_rq && cfs_rq->tg->qos_level < 0 &&
+		     !sched_idle_cpu(smp_processor_id()) &&
+		     cfs_rq->h_nr_running == cfs_rq->idle_h_nr_running)) {
+		throttle_qos_cfs_rq(cfs_rq);
+		return true;
+	}
+
+	return false;
+}
+#endif
+
 struct task_struct *
 pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 {
@@ -7091,6 +7213,16 @@ again:
 
 		se = pick_next_entity(cfs_rq, curr);
 		cfs_rq = group_cfs_rq(se);
+#ifdef CONFIG_QOS_SCHED
+		if (check_qos_cfs_rq(cfs_rq)) {
+			cfs_rq = &rq->cfs;
+			WARN(cfs_rq->nr_running == 0,
+			    "rq->nr_running=%u, cfs_rq->idle_h_nr_running=%u\n",
+			    rq->nr_running, cfs_rq->idle_h_nr_running);
+			if (unlikely(!cfs_rq->nr_running))
+				return NULL;
+		}
+#endif
 	} while (cfs_rq);
 
 	p = task_of(se);
@@ -7169,6 +7301,12 @@ idle:
 	if (new_tasks > 0)
 		goto again;
 
+#ifdef CONFIG_QOS_SCHED
+	if (unthrottle_qos_cfs_rqs(cpu_of(rq))) {
+		rq->idle_stamp = 0;
+		goto again;
+	}
+#endif
 	/*
 	 * rq is about to be idle, check if we need to update the
 	 * lost_idle_time of clock_pelt
@@ -11309,6 +11447,13 @@ void show_numa_stats(struct task_struct *p, struct seq_file *m)
 
 __init void init_sched_fair_class(void)
 {
+#ifdef CONFIG_QOS_SCHED
+	int i;
+
+	for_each_possible_cpu(i)
+		INIT_LIST_HEAD(&per_cpu(qos_throttled_cfs_rq, i));
+#endif
+
 #ifdef CONFIG_SMP
 	open_softirq(SCHED_SOFTIRQ, run_rebalance_domains);
 

Maxppddcsz/openEuler

commit 36ff048320f50ce1c7603307bd5e5bb4f87f1ba8

sched/fair: fix the semantic conflict in qos sched
jingdong inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I7SG7C

-----------------------------------------------------

1. Use qos_idle_h_nr_running as a subfunction of qos sched.

2. Use qos idle_h_nr_running as the number of offline tasks
   in check_qos_cfs_rq.

Fixes: 7e3625b1eb12c ("sched: Throttle qos cfs_rq when current cpu is running online task")
Fixes: 57b5487b858d0 ("sched/fair: Start tracking qos_offline tasks count in cfs_rq")
Signed-off-by: zhangwei123171 <zhangwei123171@jd.com>
Reviewed-by: zhaoxiaoqiang11 <zhaoxiaoqiang11@jd.com>

kernel/Kconfig.preempt
@@ -102,7 +102,7 @@ config PREEMPT_DYNAMIC
 
 config SCHED_CORE
 	bool "Core Scheduling for SMT"
-	depends on SCHED_SMT && !QOS_SCHED_SMT_EXPELLER
+	depends on SCHED_SMT && !QOS_SCHED
 	help
 	  This option permits Core Scheduling, a means of coordinated task
 	  selection across SMT siblings. When enabled -- see

kernel/sched/fair.c
@@ -4945,7 +4945,7 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 	struct cfs_bandwidth *cfs_b = tg_cfs_bandwidth(cfs_rq->tg);
 	struct sched_entity *se;
 	long task_delta, idle_task_delta, dequeue = 1;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 	long qos_idle_delta;
 #endif
 
@@ -4979,7 +4979,7 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 
 	task_delta = cfs_rq->h_nr_running;
 	idle_task_delta = cfs_rq->idle_h_nr_running;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 	qos_idle_delta = cfs_rq->qos_idle_h_nr_running;
 #endif
 
@@ -4998,7 +4998,7 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 
 		qcfs_rq->h_nr_running -= task_delta;
 		qcfs_rq->idle_h_nr_running -= idle_task_delta;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		qcfs_rq->qos_idle_h_nr_running -= qos_idle_delta;
 #endif
 
@@ -5028,7 +5028,7 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 	struct cfs_bandwidth *cfs_b = tg_cfs_bandwidth(cfs_rq->tg);
 	struct sched_entity *se;
 	long task_delta, idle_task_delta;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 	long qos_idle_delta;
 #endif
 
@@ -5059,7 +5059,7 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 
 	task_delta = cfs_rq->h_nr_running;
 	idle_task_delta = cfs_rq->idle_h_nr_running;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 	qos_idle_delta = cfs_rq->qos_idle_h_nr_running;
 #endif
 	for_each_sched_entity(se) {
@@ -5070,7 +5070,7 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 
 		cfs_rq->h_nr_running += task_delta;
 		cfs_rq->idle_h_nr_running += idle_task_delta;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		cfs_rq->qos_idle_h_nr_running += qos_idle_delta;
 #endif
 
@@ -5087,7 +5087,7 @@ void unthrottle_cfs_rq(struct cfs_rq *cfs_rq)
 
 		cfs_rq->h_nr_running += task_delta;
 		cfs_rq->idle_h_nr_running += idle_task_delta;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		cfs_rq->qos_idle_h_nr_running += qos_idle_delta;
 #endif
 
@@ -5723,7 +5723,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
 	int idle_h_nr_running = task_has_idle_policy(p);
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 	int qos_idle_h_nr_running = task_has_qos_idle_policy(p);
 #endif
 	int task_new = !(flags & ENQUEUE_WAKEUP);
@@ -5753,7 +5753,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 		cfs_rq->h_nr_running++;
 		cfs_rq->idle_h_nr_running += idle_h_nr_running;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		cfs_rq->qos_idle_h_nr_running += qos_idle_h_nr_running;
 #endif
 
@@ -5773,7 +5773,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 		cfs_rq->h_nr_running++;
 		cfs_rq->idle_h_nr_running += idle_h_nr_running;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		cfs_rq->qos_idle_h_nr_running += qos_idle_h_nr_running;
 #endif
 
@@ -5850,7 +5850,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
 	int idle_h_nr_running = task_has_idle_policy(p);
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 	int qos_idle_h_nr_running = task_has_qos_idle_policy(p);
 #endif
 	unsigned int prev_nr = rq->cfs.h_nr_running;
@@ -5864,7 +5864,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 		cfs_rq->h_nr_running--;
 		cfs_rq->idle_h_nr_running -= idle_h_nr_running;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		cfs_rq->qos_idle_h_nr_running -= qos_idle_h_nr_running;
 #endif
 
@@ -5896,7 +5896,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 		cfs_rq->h_nr_running--;
 		cfs_rq->idle_h_nr_running -= idle_h_nr_running;
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 		cfs_rq->qos_idle_h_nr_running -= qos_idle_h_nr_running;
 #endif
 
@@ -7787,6 +7787,14 @@ static __always_inline int unthrottle_qos_cfs_rqs(int cpu)
 	return res;
 }
 
+static bool qos_sched_idle_cpu(int this_cpu)
+{
+	struct rq *rq = cpu_rq(this_cpu);
+
+	return unlikely(rq->nr_running &&
+			rq->nr_running == rq->cfs.qos_idle_h_nr_running);
+}
+
 static __always_inline bool check_qos_cfs_rq(struct cfs_rq *cfs_rq)
 {
 	if (!qos_sched_enabled())
@@ -7797,7 +7805,7 @@ static __always_inline bool check_qos_cfs_rq(struct cfs_rq *cfs_rq)
 	}
 
 	if (unlikely(cfs_rq && is_offline_level(cfs_rq->tg->qos_level) &&
-		!sched_idle_cpu(smp_processor_id()) &&
+		!qos_sched_idle_cpu(smp_processor_id()) &&
 		cfs_rq->h_nr_running == cfs_rq->idle_h_nr_running)) {
 		throttle_qos_cfs_rq(cfs_rq);
 		return true;
@@ -7953,14 +7961,6 @@ static bool qos_smt_check_siblings_status(int this_cpu)
 	}
 
 	return false;
-}
-
-static bool qos_sched_idle_cpu(int this_cpu)
-{
-	struct rq *rq = cpu_rq(this_cpu);
-
-	return unlikely(rq->nr_running == rq->cfs.qos_idle_h_nr_running &&
-			rq->nr_running);
 }
 
 static __always_inline bool qos_smt_expelled(int this_cpu)

kernel/sched/sched.h
@@ -655,7 +655,7 @@ struct cfs_rq {
 	unsigned int		forceidle_seq;
 	KABI_FILL_HOLE(unsigned int kabi_hole)
 	u64			min_vruntime_fi;
-#elif defined CONFIG_QOS_SCHED_SMT_EXPELLER && !defined(__GENKSYMS__)
+#elif defined CONFIG_QOS_SCHED && !defined(__GENKSYMS__)
 	union {
 		unsigned int            qos_idle_h_nr_running; /* qos_level：-1 */
 		unsigned long           qos_idle_h_nr_running_padding;
@@ -3090,11 +3090,10 @@ static inline int is_expeller_level(struct task_group *tg)
 }
 #endif
 
-#ifdef CONFIG_QOS_SCHED_SMT_EXPELLER
+#ifdef CONFIG_QOS_SCHED
 static __always_inline int task_has_qos_idle_policy(struct task_struct *p)
 {
-	if (!qos_sched_enabled() ||
-	    !static_branch_likely(&qos_smt_expell_switch))
+	if (!qos_sched_enabled())
 		return 0;
 
 	return qos_idle_policy(task_group(p)->qos_level) && p->policy == SCHED_IDLE;