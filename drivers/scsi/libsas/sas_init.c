038f5fd0c82158d23c2bdcb3b2482c44df30ef0c
xiabing <xiabing12@h-partners.com>
2023-08-31 09:32:46 +0800

    scsi: libsas: Revert "Export-sas_phy_enable"
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I7WQZ1
    CVE: NA
    
    ----------------------------------------------------------------------
    
    This reverts commit 798b261ba08c3e51fd84b313f9f17c2e6f43e6e3.
    
    Signed-off-by: xiabing <xiabing12@h-partners.com>

--- drivers/scsi/libsas/sas_init.c
+++ drivers/scsi/libsas/sas_init.c
@@ -252,7 +252,7 @@ static int transport_sas_phy_reset(struct sas_phy *phy, int hard_reset)
 	}
 }
 
-int sas_phy_enable(struct sas_phy *phy, int enable)
+static int sas_phy_enable(struct sas_phy *phy, int enable)
 {
 	int ret;
 	enum phy_func cmd;
@@ -284,7 +284,6 @@ int sas_phy_enable(struct sas_phy *phy, int enable)
 	}
 	return ret;
 }
-EXPORT_SYMBOL_GPL(sas_phy_enable);
 
 int sas_phy_reset(struct sas_phy *phy, int hard_reset)
 {
