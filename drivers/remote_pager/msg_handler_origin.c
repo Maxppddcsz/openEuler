openeuler-mirror/kernel

commit bb659473cc9ff43b0f32d2193905a4121df14c2e

drivers: remote_pager: introduce remote_pager module for gmem
euleros inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I7WLVX

---------------------------------------------

Introduce remote_pager, an extension module for GMEM,
which can be used for communication between the remote and host

Signed-off-by: Cunshu Ni <nicunshu@huawei.com>

drivers/remote_pager/msg_handler_origin.c
@@ -1,0 +1,476 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Generalized Memory Management.
+ *
+ * Copyright (c) 2023- Huawei, Inc.
+ * Author: Bin Wang
+ * Co-Author: Chunsheng Luo, Cunshu Ni
+ *
+ */
+#include <linux/slab.h>
+#include <linux/gmem.h>
+#include <linux/sched/mm.h>
+#include <linux/vm_object.h>
+#include <linux/dma-mapping.h>
+#include <linux/version.h>
+
+#include "msg_handler.h"
+#include "wait_station.h"
+#include "svm_proc_mng.h"
+
+#define NPU_PAGE_SIZE PAGE_SIZE
+#define MAX_NR_NPU 8
+#define GMEM_DEBUG 0
+
+static gm_dev_t *gm_devs[MAX_NR_NPU];
+
+gm_dev_t *gmem_id_to_device(unsigned int id)
+{
+	if (id >= MAX_NR_NPU) {
+		pr_err("device id is invalid. (dev_id = %u)\n", id);
+		return NULL;
+	}
+
+	return gm_devs[id];
+}
+
+int gmem_register_pair_remote_task(int origin_nid, int origin_pid, int remote_nid, int remote_pid)
+{
+	struct gm_pair_msg_rq req;
+	struct comm_msg_rsp *rsp;
+	int ret = 0;
+	struct wait_station *ws;
+
+	/* open msg chan */
+	pr_err("%s origin_nid %d, origin_pid %d, remote_nid %d, remote_pid %d\n", __func__,
+	origin_nid, origin_pid, remote_nid, remote_pid);
+	ret = msg_open(remote_nid);
+	if (ret < 0) {
+		pr_err("%s open msg chan failed\n", __func__);
+		return ret;
+	}
+
+	/* start pairing */
+	ws = get_wait_station();
+	req.my_pid = origin_pid;
+	req.my_ws = ws->id;
+	req.peer_nid = remote_nid;
+	req.peer_pid = remote_pid;
+
+	ret = msg_send_nid(GMEM_TASK_PAIRING_REQUEST, origin_nid,
+					   remote_nid, &req, sizeof(struct gm_pair_msg_rq));
+	rsp = wait_at_station(ws);
+	if ((long)rsp != -ETIMEDOUT) {
+		ret = rsp->ret;
+		kfree(rsp);
+		gmem_add_to_svm_proc(origin_nid, origin_pid, remote_nid, remote_pid);
+	}
+
+	return ret;
+}
+EXPORT_SYMBOL(gmem_register_pair_remote_task);
+
+int gmem_handle_dev_fault(struct rpg_kmsg_message *msg)
+{
+	int ret;
+	struct gm_pager_msg_rq *recv = (struct gm_pager_msg_rq *)msg;
+	unsigned int my_pid = recv->peer_pid;
+	unsigned int nid = recv->header.to_nid;
+	unsigned int peer_nid = recv->header.from_nid;
+	unsigned int peer_ws = recv->my_ws;
+	gm_dev_t *dev = gm_devs[peer_nid];
+	struct task_struct *tsk;
+	struct mm_struct *mm;
+
+	tsk = find_get_task_by_vpid(my_pid);
+	if (!tsk) {
+		pr_err("svm process does not have task_struct\n");
+		ret = GM_RET_FAILURE_UNKNOWN;
+		goto out;
+	}
+
+	mm = get_task_mm(tsk);
+	if (!mm) {
+		pr_err("no mm\n");
+		ret = GM_RET_FAILURE_UNKNOWN;
+		goto put_task;
+	}
+
+	if (!dev) {
+		pr_info("gmem: device get failed, dev_id %ld\n", (unsigned long)peer_nid);
+		ret = -ENODEV;
+		goto put_mm;
+	}
+
+	ret = gm_dev_fault(mm, recv->va, dev, 0);
+	if (ret != GM_RET_SUCCESS && ret != GM_RET_PAGE_EXIST) {
+		pr_info("gmem dev fault failed\n");
+		ret = -EFAULT;
+		goto put_mm;
+	}
+
+put_mm:
+	mmput(mm);
+put_task:
+	put_task_struct(tsk);
+out:
+	gmem_send_comm_msg_reply(nid, peer_nid, peer_ws, ret);
+	kfree(msg);
+	return ret;
+}
+
+gm_ret_t gmem_map(struct gm_fault_t *gmf)
+{
+	int ret = 0;
+	struct wait_station *ws;
+	struct comm_msg_rsp *rsp;
+	struct mm_struct *mm = gmf->mm;
+	struct svm_proc *proc = search_svm_proc_by_mm(mm);
+	struct gm_pager_msg_rq req = {
+		.peer_pid = proc->peer_pid,
+		.va = gmf->va,
+		.size = gmf->size,
+		.behavior = gmf->behavior
+	};
+
+	if (!proc) {
+		pr_err("can not find proc\n");
+		return -EBUSY;
+	}
+
+	ws = get_wait_station();
+	req.my_ws = ws->id;
+
+	if (gmf->copy) {
+		req.flags |= GMEM_COPY_PAGE;
+		req.dma_addr = gmf->dma_addr;
+	}
+
+	ret = msg_send_nid(GMEM_ALLOC_PAGE_REQUEST, proc->nid, proc->peer_nid,
+			&req, sizeof(struct gm_pager_msg_rq));
+	rsp = wait_at_station(ws);
+	if ((long)rsp == -ETIMEDOUT)
+		return -EBUSY;
+	ret |= rsp->ret;
+	kfree(rsp);
+	if (ret) {
+		if (ret == GM_RET_MIGRATING) {
+			pr_info("gmem: race with migrating\n");
+			return ret;
+		} else {
+			pr_info("send alloc page message failed %d\n", ret);
+			return GM_RET_FAILURE_UNKNOWN;
+		}
+	}
+
+	return GM_RET_SUCCESS;
+}
+
+gm_ret_t gmem_unmap(struct gm_fault_t *gmf)
+{
+	int ret;
+	struct wait_station *ws;
+	struct comm_msg_rsp *rsp;
+	struct mm_struct *mm = gmf->mm;
+	struct svm_proc *proc = search_svm_proc_by_mm(mm);
+	struct gm_pager_msg_rq req = {
+		.peer_pid = proc->peer_pid,
+		.va = gmf->va,
+		.size = gmf->size,
+	};
+
+	if (!proc) {
+		pr_err("can not find proc\n");
+		return -EBUSY;
+	}
+
+	if (gmf->copy) {
+		req.flags |= GMEM_COPY_PAGE;
+		req.dma_addr = gmf->dma_addr;
+	}
+
+	ws = get_wait_station();
+	req.my_ws = ws->id;
+
+	ret = msg_send_nid(GMEM_FREE_PAGE_REQUEST, proc->nid, proc->peer_nid,
+						&req, sizeof(struct gm_pager_msg_rq));
+	rsp = wait_at_station(ws);
+	if ((long)rsp == -ETIMEDOUT)
+		return -EBUSY;
+	ret |= rsp->ret;
+	kfree(rsp);
+	if (ret) {
+		pr_info("send free page message failed %d\n", ret);
+		return GM_RET_FAILURE_UNKNOWN;
+	}
+
+	return GM_RET_SUCCESS;
+}
+
+gm_ret_t gmem_alloc(struct gm_fault_t *gmf)
+{
+	int ret = 0;
+	struct wait_station *ws;
+	struct comm_msg_rsp *rsp;
+	struct mm_struct *mm = gmf->mm;
+	struct svm_proc *proc = search_svm_proc_by_mm(mm);
+	struct gm_pager_msg_rq req = {
+		.peer_pid = proc->peer_pid,
+		.va = gmf->va,
+		.size = gmf->size,
+		.prot = gmf->prot,
+	};
+
+	if (!proc) {
+		pr_err("can not find proc\n");
+		return -EBUSY;
+	}
+
+	ws = get_wait_station();
+	req.my_ws = ws->id;
+	ret = msg_send_nid(GMEM_ALLOC_VMA_REQUEST, proc->nid, proc->peer_nid,
+						&req, sizeof(struct gm_pager_msg_rq));
+	rsp = wait_at_station(ws);
+	if ((long)rsp == -ETIMEDOUT)
+		return -EBUSY;
+	ret |= rsp->ret;
+	kfree(rsp);
+	if (ret) {
+		pr_info("send alloc vma message failed %d\n", ret);
+		return GM_RET_NOMEM;
+	}
+
+	return GM_RET_SUCCESS;
+}
+
+gm_ret_t gmem_free(struct gm_fault_t *gmf)
+{
+	int ret = 0;
+	struct wait_station *ws;
+	struct comm_msg_rsp *rsp;
+	struct mm_struct *mm = gmf->mm;
+	struct svm_proc *proc = search_svm_proc_by_mm(mm);
+	struct gm_pager_msg_rq req = {
+		.peer_pid = proc->peer_pid,
+		.va = gmf->va,
+		.size = gmf->size,
+	};
+
+	if (!proc) {
+		pr_err("can not find proc\n");
+		return -EBUSY;
+	}
+
+	ws = get_wait_station();
+	req.my_ws = ws->id;
+	ret = msg_send_nid(GMEM_FREE_VMA_REQUEST, proc->nid, proc->peer_nid,
+						&req, sizeof(struct gm_pager_msg_rq));
+	rsp = wait_at_station(ws);
+	if ((long)rsp == -ETIMEDOUT)
+		return -EBUSY;
+	ret |= rsp->ret;
+	kfree(rsp);
+	if (ret) {
+		pr_info("send free vma message failed %d\n", ret);
+		return GM_RET_FAILURE_UNKNOWN;
+	}
+
+	return GM_RET_SUCCESS;
+}
+
+int gmem_handle_evict_page(struct rpg_kmsg_message *msg)
+{
+	struct gm_evict_page_msg_rq *recv = (struct gm_evict_page_msg_rq *)msg;
+	unsigned int nid = recv->header.to_nid;
+	unsigned int peer_nid = recv->header.from_nid;
+	unsigned int peer_ws = recv->ws;
+	unsigned int pid = recv->peer_pid;
+	unsigned long size = recv->size;
+	unsigned long addr = recv->va;
+	struct vm_area_struct *vma;
+	struct page *page;
+	dma_addr_t dma_addr;
+	gm_mapping_t *gm_page;
+	struct device *dma_dev;
+	struct gm_fault_t gmf;
+	struct svm_proc *proc;
+	struct task_struct *tsk;
+	struct mm_struct *mm;
+	int ret;
+	struct folio *folio = NULL;
+
+	proc = search_svm_proc_by_pid(pid);
+	if (!proc) {
+		pr_err("can not find svm_proc of task-%d\n", pid);
+		ret = -EINVAL;
+		goto response;
+	}
+
+	tsk = find_get_task_by_vpid(pid);
+	if (!tsk) {
+		pr_err("can not find task of task-%d\n", pid);
+		ret = -EINVAL;
+		goto response;
+	}
+
+	mm = get_task_mm(tsk);
+	if (!mm) {
+		pr_err("task-%d exited\n", pid);
+		ret = -EINTR;
+		goto put_task;
+	}
+
+	if (mm != proc->mm) {
+		pr_err("miss match\n");
+		ret = -EINTR;
+		goto put_mm;
+	}
+
+	gmf.mm = mm;
+	gmf.va = addr;
+	gmf.size = size;
+	gmf.copy = GMEM_COPY_PAGE;
+
+	vma = find_vma(mm, addr);
+	if (!vma || !vma->vm_obj) {
+		pr_err("evict addr %lx vma %lx vm_obj %lx, no vma or vm_obj\n", addr,
+			(unsigned long)vma, vma ? (unsigned long)vma->vm_obj : 0);
+		ret = -EINVAL;
+		goto put_mm;
+	}
+
+	gm_page = vm_object_lookup(vma->vm_obj, addr);
+	if (!gm_page) {
+		pr_err("evictim gm_page is NULL\n");
+		ret = -EINVAL;
+		goto put_mm;
+	}
+
+	mutex_lock(&gm_page->lock);
+	if (gm_mapping_willneed(gm_page)) {
+		pr_info("gmem: racing with prefetch or willneed so cancel evict\n");
+		clear_gm_mapping_willneed(gm_page);
+		ret = -EINVAL;
+		goto unlock;
+	}
+
+	if (!gm_mapping_device(gm_page)) {
+		pr_info("gmem: page is not in device\n");
+		ret = -EINVAL;
+		goto unlock;
+	}
+
+	if (size == HPAGE_PMD_SIZE) {
+		folio = vma_alloc_folio(GFP_TRANSHUGE, HPAGE_PMD_ORDER, vma, addr, true);
+		page = &folio->page;
+	} else {
+		page = alloc_page(GFP_KERNEL);
+	}
+
+	if (!page) {
+		pr_err("gmem: gmem_evict_page alloc hugepage failed\n");
+		ret = -ENOMEM;
+		goto unlock;
+	}
+
+	dma_dev = gm_page->dev->dma_dev;
+	dma_addr = dma_map_page(dma_dev, page, 0, size, DMA_BIDIRECTIONAL);
+	gmf.dev = gm_page->dev;
+	gmf.dma_addr = dma_addr;
+
+	ret = gmem_unmap(&gmf);
+	dma_unmap_page(dma_dev, dma_addr, size, DMA_BIDIRECTIONAL);
+	if (ret) {
+		pr_err("gmem_unmap failed, ret %d\n", ret);
+		put_page(page);
+		goto unlock;
+	}
+
+	set_gm_mapping_host(gm_page, page);
+
+unlock:
+	mutex_unlock(&gm_page->lock);
+put_mm:
+	mmput(mm);
+put_task:
+	put_task_struct(tsk);
+response:
+	gmem_send_comm_msg_reply(nid, peer_nid, peer_ws, ret);
+	kfree(msg);
+	return ret;
+}
+
+gm_ret_t gmem_create(gm_dev_t *dev, void **pmap)
+{
+	return GM_RET_SUCCESS;
+}
+
+gm_mmu_t gm_mmu = {
+	.peer_va_alloc_fixed = gmem_alloc,
+	.pmap_create = gmem_create,
+	.peer_va_free = gmem_free,
+	.peer_map = gmem_map,
+	.peer_unmap = gmem_unmap,
+};
+
+#define ASCEND910_HBM_START 0x0000000800000000
+#define ASCEND910_HBM_END   0x0000000fffffffff
+
+gm_ret_t mmu_dev_create(struct device *dev, int devid)
+{
+	gm_ret_t ret;
+
+	ret = gm_dev_create(&gm_mmu, NULL, GM_DEV_CAP_REPLAYABLE | GM_DEV_CAP_PEER, &dev->gm_dev);
+	if (ret != GM_RET_SUCCESS) {
+		pr_err("NPU gmem device create failed\n");
+		return ret;
+	}
+
+	ret = gm_dev_register_physmem(dev->gm_dev, ASCEND910_HBM_START, ASCEND910_HBM_END);
+	if (ret != GM_RET_SUCCESS) {
+		pr_err("NPU gmem device register physical memory failed\n");
+		goto free_gm_dev;
+	}
+
+	dev->gm_dev->dma_dev = dev;
+	gm_devs[devid] = dev->gm_dev;
+
+	pr_info("Create NPU gmem device and register HBM\n");
+	return ret;
+free_gm_dev:
+	gm_dev_destroy(dev->gm_dev);
+	dev->gm_dev = NULL;
+	return ret;
+}
+EXPORT_SYMBOL(mmu_dev_create);
+
+gm_ret_t mmu_as_attach(struct device *dev)
+{
+	gm_ret_t ret;
+	gm_dev_t *gm_dev = dev->gm_dev;
+	gm_context_t *gm_ctx;
+
+	if (!gm_dev) {
+		pr_err("NPU device gm_dev is NULL\n");
+		return GM_RET_FAILURE_UNKNOWN;
+	}
+
+	if (!current->mm->gm_as) {
+		ret = gm_as_create(0, ULONG_MAX, GM_AS_ALLOC_DEFAULT, NPU_PAGE_SIZE,
+						   &current->mm->gm_as);
+		if (ret != GM_RET_SUCCESS) {
+			pr_err("Process %d create gm_as failed\n", current->pid);
+			return ret;
+		}
+	}
+
+	ret = gm_as_attach(current->mm->gm_as, gm_dev, 0, 1, &gm_ctx);
+	if (ret != GM_RET_SUCCESS) {
+		pr_err("gm_dev attach to process %d failed\n", current->pid);
+		return ret;
+	}
+
+	pr_info("Attach gm_dev to process %d\n", current->pid);
+	return ret;
+}
+EXPORT_SYMBOL(mmu_as_attach);

drivers/Kconfig
@@ -243,4 +243,6 @@ source "drivers/hte/Kconfig"
 
 source "drivers/cdx/Kconfig"
 
+source "drivers/remote_pager/Kconfig"
+
 endmenu

drivers/Makefile
@@ -195,3 +195,4 @@ obj-$(CONFIG_PECI)		+= peci/
 obj-$(CONFIG_HTE)		+= hte/
 obj-$(CONFIG_DRM_ACCEL)		+= accel/
 obj-$(CONFIG_CDX_BUS)		+= cdx/
+obj-$(CONFIG_REMOTE_PAGER)	+= remote_pager/

drivers/remote_pager/Kconfig
@@ -1,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0
+
+menu "remote pager device"
+
+config REMOTE_PAGER
+	tristate "remote pager"
+	default m
+	depends on GMEM
+	help
+		If unsure, say Y.
+
+config REMOTE_PAGER_MASTER
+	tristate "remote pager master"
+	default m
+	depends on REMOTE_PAGER
+	help
+		If unsure, say Y.
+
+endmenu

drivers/remote_pager/Makefile
@@ -1,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+
+obj-$(CONFIG_REMOTE_PAGER) += remote_pager.o
+
+remote_pager-$(CONFIG_REMOTE_PAGER) :=	main.o \
+					wait_station.o \
+					msg_handler_comm.o \
+					msg_chan/msg_layer/msg_layer.o \
+					svm_proc_mng.o
+
+remote_pager-$(CONFIG_REMOTE_PAGER_MASTER) += msg_handler_origin.o

drivers/remote_pager/main.c
@@ -1,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Generalized Memory Management.
+ *
+ * Copyright (c) 2023- Huawei, Inc.
+ * Author: Chunsheng Luo
+ * Co-Author: Jun Chen
+ */
+#include <linux/module.h>
+#include "msg_chan/msg_layer/msg_layer.h"
+#include "msg_handler.h"
+
+#ifdef CONFIG_REMOTE_PAGER_SLAVE
+#include "swap/device/swap_manager.h"
+#endif
+
+static int __init remote_pager_init(void)
+{
+	msg_handle_init();
+
+#ifdef CONFIG_REMOTE_PAGER_SLAVE
+	init_swap_manager("list_lru");
+#endif
+	return 0;
+}
+
+static void __exit remote_pager_exit(void)
+{
+	/*
+	 * If module_init() is implemented, module_exit()
+	 * should be implemented as well.
+	 */
+}
+
+module_init(remote_pager_init);
+module_exit(remote_pager_exit);
+
+MODULE_AUTHOR("Huawei Tech. Co., Ltd.");
+MODULE_DESCRIPTION("Remote-pager");
+MODULE_ALIAS("Remote-pager");
+MODULE_LICENSE("GPL");
+

drivers/remote_pager/msg_chan/msg_layer/msg_layer.c
@@ -1,0 +1,271 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Generalized Memory Management.
+ *
+ * Copyright (c) 2023- Huawei, Inc.
+ * Author: Chunsheng Luo
+ * Co-Author: Jiangtian Feng, Jun Chen
+ */
+#include <linux/module.h>
+#include <linux/kthread.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+
+#include "msg_layer.h"
+
+#define MAX_NUM_NODES	16
+#define MSG_SLEEP_MIN	2
+#define MSG_SLEEP_MAX	3
+
+/* Per-node handle */
+struct sock_handle {
+	int nid;
+	int status;
+	int chan_id;
+	struct task_struct *recv_handler;
+};
+
+static struct sock_handle sock_handles[MAX_NUM_NODES];
+static struct phys_channel_ops *g_phys_chan_ops;
+
+int msg_send(int chan_id, void *msg_data, size_t msg_len)
+{
+	int ret = 0;
+
+	if (!g_phys_chan_ops)
+		return -ENOENT;
+
+	ret = g_phys_chan_ops->copy_to(chan_id, msg_data, msg_len, 1);
+	ret |= g_phys_chan_ops->notify(chan_id);
+
+	return ret;
+}
+
+static inline int build_msg(int type, int from_nid, int to_nid, void *msg_data, size_t msg_len)
+{
+	struct rpg_kmsg_message *msg = (struct rpg_kmsg_message *)msg_data;
+
+	msg->header.type = type;
+	msg->header.prio = RPG_KMSG_PRIO_NORMAL;
+	msg->header.size = msg_len;
+	msg->header.from_nid = from_nid;
+	msg->header.to_nid = to_nid;
+
+	return 0;
+}
+
+int msg_send_nid(int type, int from_nid, int to_nid, void *msg_data, size_t msg_len)
+{
+	struct sock_handle *sh = sock_handles + to_nid;
+
+	build_msg(type, from_nid, to_nid, msg_data, msg_len);
+
+	return msg_send(sh->chan_id, msg_data, msg_len);
+}
+EXPORT_SYMBOL(msg_send_nid);
+
+int msg_recv(int chan_id, void *buf, size_t len)
+{
+	if (!g_phys_chan_ops)
+		return -ENOENT;
+
+	return g_phys_chan_ops->copy_from(chan_id, buf, len, 1);
+}
+
+extern int handle_remote_pager_work(void *msg);
+static int recv_handler(void *arg)
+{
+	struct sock_handle *sh = arg;
+
+	log_info("RECV handler for %d is ready ha %ld\n", sh->nid, sizeof(struct rpg_kmsg_hdr));
+
+	while (!kthread_should_stop()) {
+		size_t len;
+		int ret;
+		size_t offset;
+		struct rpg_kmsg_hdr header;
+		char *data = NULL;
+		size_t msg_len = 0;
+
+		/* compose header */
+		offset = 0;
+		len = sizeof(header);
+		while (len > 0) {
+			ret = msg_recv(sh->chan_id, (char *)(&header) + offset, len);
+			if (ret == -ENOENT) {
+				pr_err("no msg chan failed\n");
+				usleep_range(MSG_SLEEP_MIN, MSG_SLEEP_MAX);
+				break;
+			}
+
+			if ((ret == -1) || kthread_should_stop())
+				return 0;
+
+			offset += ret;
+			len -= ret;
+		}
+
+		if (ret < 0)
+			break;
+
+		msg_len = header.size;
+		if (!msg_len) {
+			pr_err("msg_len is zero failed? from_nid %d prio:%d type:%d size:%ld\n",
+					header.from_nid, header.prio, header.type, header.size);
+			continue;
+		}
+
+		/* compose body */
+		data = kmalloc(msg_len, GFP_KERNEL);
+		BUG_ON(!data && "Unable to alloc a message");
+		memcpy(data, &header, sizeof(header));
+
+		offset = sizeof(header);
+		len = msg_len - offset;
+
+		while (len > 0) {
+			ret = msg_recv(sh->chan_id, data + offset, len);
+			if (ret == -1 || kthread_should_stop())
+				return 0;
+
+			offset += ret;
+			len -= ret;
+		}
+
+		if (ret < 0)
+			break;
+
+		/* Call pcn_kmsg upper layer */
+		handle_remote_pager_work(data);
+	}
+
+	return 0;
+}
+
+int msg_open(int nid)
+{
+	int chan_id = 0;
+	struct sock_handle *sh = sock_handles + nid;
+	struct task_struct *tsk_recv;
+
+	if (sh->status == MSG_CHAN_ENABLE) {
+		pr_err("node:%d msg chan is enabled\n", nid);
+		return 0;
+	}
+
+	if (!g_phys_chan_ops)
+		return -ENOENT;
+
+	chan_id = g_phys_chan_ops->open(nid);
+	if (chan_id < 0) {
+		log_err("open msg channel failed %d\n", chan_id);
+		return chan_id;
+	}
+
+	tsk_recv = kthread_run(recv_handler, sock_handles + nid, "remote-pager-recv");
+	if (IS_ERR(tsk_recv)) {
+		log_err("Cannot create %s handler, %ld\n", "remote-pager-recv", PTR_ERR(tsk_recv));
+		return PTR_ERR(tsk_recv);
+	}
+
+	sh->chan_id = chan_id;
+	sh->status = MSG_CHAN_ENABLE;
+	sh->nid = nid;
+	sh->recv_handler = tsk_recv;
+
+	pr_err("%s chanid %d\n", __func__, chan_id);
+
+	return chan_id;
+}
+EXPORT_SYMBOL(msg_open);
+
+int msg_close(int nid)
+{
+	struct sock_handle *sh = sock_handles + nid;
+
+	/* TODO: Get sock_handle, then set sock_handle disable and destroy recv task */
+	if (sh->status != MSG_CHAN_ENABLE) {
+		pr_err("node:%d msg chan is disabled\n", nid);
+		return 0;
+	}
+
+	if (sh->recv_handler) {
+		kthread_stop(sh->recv_handler);
+		sh->recv_handler = NULL;
+	}
+
+	if (g_phys_chan_ops)
+		g_phys_chan_ops->close(sh->chan_id);
+
+	sh->chan_id = 0;
+	sh->status = MSG_CHAN_DISABLE;
+
+	return 0;
+}
+EXPORT_SYMBOL(msg_close);
+
+int handle_migrate_page(void *peer_addr, struct page *local_page, size_t size, int dir)
+{
+	if (!g_phys_chan_ops)
+		return -ENOENT;
+
+	return g_phys_chan_ops->migrate_page(peer_addr, local_page, size, dir);
+}
+EXPORT_SYMBOL(handle_migrate_page);
+
+static DEFINE_SPINLOCK(install_lock);
+static int default_msg_chan_id;
+int msg_layer_install_phy_ops(struct phys_channel_ops *ops, int default_chan_id)
+{
+	int ret = 0;
+
+	if (!ops) {
+		pr_err("install NULL as msg channel\n");
+		return -EINVAL;
+	}
+
+	spin_lock(&install_lock);
+	if (g_phys_chan_ops) {
+		ret = -EEXIST;
+		pr_err("phy_ops areadly be installed\n");
+		goto unlock;
+	}
+
+	/* must before msg_open */
+	g_phys_chan_ops = ops;
+	if (default_chan_id >= 0) {
+		ret = msg_open(default_chan_id);
+		if (ret) {
+			pr_err("can not open msg channel %d\n", default_chan_id);
+			g_phys_chan_ops = NULL;
+			goto unlock;
+		}
+	}
+
+	default_msg_chan_id = default_chan_id;
+
+unlock:
+	spin_unlock(&install_lock);
+	return ret;
+}
+EXPORT_SYMBOL(msg_layer_install_phy_ops);
+
+int msg_layer_uninstall_phy_ops(struct phys_channel_ops *ops)
+{
+	if (!ops || ops != g_phys_chan_ops) {
+		pr_err("Invalid phy_ops\n");
+		return -EINVAL;
+	}
+
+	spin_lock(&install_lock);
+	if (default_msg_chan_id >= 0)
+		msg_close(default_msg_chan_id);
+
+	g_phys_chan_ops = NULL;
+	default_msg_chan_id = -1;
+	spin_unlock(&install_lock);
+
+	return 0;
+}
+EXPORT_SYMBOL(msg_layer_uninstall_phy_ops);
+MODULE_LICENSE("GPL");

drivers/remote_pager/msg_chan/msg_layer/msg_layer.h
@@ -1,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Generalized Memory Management.
+ *
+ * Copyright (c) 2023- Huawei, Inc.
+ * Author: Chunsheng Luo
+ * Co-Author: Jiangtian Feng
+ */
+#ifndef __MSG_LAYER_H__
+#define __MSG_LAYER_H__
+
+#include <linux/string.h>
+#include <linux/remote_pager/msg_chan.h>
+
+#define RPG_KMSG_MAX_SIZE (64UL << 10)
+#define RPG_KMSG_MAX_PAYLOAD_SIZE \
+	(RPG_KMSG_MAX_SIZE - sizeof(struct rpg_kmsg_hdr))
+
+/* Enumerate message priority. XXX Priority is not supported yet. */
+enum rpg_kmsg_prio {
+	RPG_KMSG_PRIO_LOW,
+	RPG_KMSG_PRIO_NORMAL,
+	RPG_KMSG_PRIO_HIGH,
+};
+
+#define MSG_CHAN_DISABLE 0
+#define MSG_CHAN_ENABLE 1
+
+struct rpg_kmsg_hdr {
+	int from_nid		:6;
+	int to_nid		:6;
+	enum rpg_kmsg_prio prio	:2;
+	int type		:8;
+	size_t size;
+} __packed;
+
+struct rpg_kmsg_message {
+	struct rpg_kmsg_hdr header;
+	unsigned char data[RPG_KMSG_MAX_PAYLOAD_SIZE];
+} __packed;
+
+int msg_send_nid(int type, int from_nid, int to_nid, void *msg_data, size_t msg_len);
+int msg_send(int chan_id, void *msg_data, size_t msg_len);
+int msg_recv(int chan_id, void *buf, size_t len);
+int msg_open(int nid);
+int msg_close(int nid);
+int handle_migrate_page(void *peer_addr, struct page *local_page, size_t size, int dir);
+
+#endif

Maxppddcsz/openEuler

commit 4ca5d5fb18c0549553d323dba1e353190786f9df

remote_pager: assign peer_pid after checking if svm_proc is NULL euleros inclusion category: bugfix bugzilla: https://gitee.com/openeuler/kernel/issues/I7WLVX
---------------------------------------------

assign peer_pid after checking if svm_proc is NULL.

Fixes: bb659473cc9 ("drivers: remote_pager: introduce remote_pager module for gmem")

Signed-off-by: Jiangtian Feng <fengjiangtian@huawei.com>

drivers/remote_pager/msg_handler_origin.c
@@ -127,7 +127,6 @@ gm_ret_t gmem_map(struct gm_fault_t *gmf)
 	struct mm_struct *mm = gmf->mm;
 	struct svm_proc *proc = search_svm_proc_by_mm(mm);
 	struct gm_pager_msg_rq req = {
-		.peer_pid = proc->peer_pid,
 		.va = gmf->va,
 		.size = gmf->size,
 		.behavior = gmf->behavior
@@ -138,6 +137,7 @@ gm_ret_t gmem_map(struct gm_fault_t *gmf)
 		return -EBUSY;
 	}
 
+	req.peer_pid = proc->peer_pid;
 	ws = get_wait_station();
 	req.my_ws = ws->id;
 
@@ -174,7 +174,6 @@ gm_ret_t gmem_unmap(struct gm_fault_t *gmf)
 	struct mm_struct *mm = gmf->mm;
 	struct svm_proc *proc = search_svm_proc_by_mm(mm);
 	struct gm_pager_msg_rq req = {
-		.peer_pid = proc->peer_pid,
 		.va = gmf->va,
 		.size = gmf->size,
 	};
@@ -184,6 +183,7 @@ gm_ret_t gmem_unmap(struct gm_fault_t *gmf)
 		return -EBUSY;
 	}
 
+	req.peer_pid = proc->peer_pid;
 	if (gmf->copy) {
 		req.flags |= GMEM_COPY_PAGE;
 		req.dma_addr = gmf->dma_addr;
@@ -215,7 +215,6 @@ gm_ret_t gmem_alloc(struct mm_struct *mm, unsigned long va, unsigned long size,
 	struct comm_msg_rsp *rsp;
 	struct svm_proc *proc = search_svm_proc_by_mm(mm);
 	struct gm_pager_msg_rq req = {
-		.peer_pid = proc->peer_pid,
 		.va = va,
 		.size = size,
 		.prot = prot,
@@ -226,6 +225,7 @@ gm_ret_t gmem_alloc(struct mm_struct *mm, unsigned long va, unsigned long size,
 		return -EBUSY;
 	}
 
+	req.peer_pid = proc->peer_pid;
 	ws = get_wait_station();
 	req.my_ws = ws->id;
 	ret = msg_send_nid(GMEM_ALLOC_VMA_REQUEST, proc->nid, proc->peer_nid,
@@ -250,7 +250,6 @@ gm_ret_t gmem_free(struct mm_struct *mm, unsigned long va, unsigned long size)
 	struct comm_msg_rsp *rsp;
 	struct svm_proc *proc = search_svm_proc_by_mm(mm);
 	struct gm_pager_msg_rq req = {
-		.peer_pid = proc->peer_pid,
 		.va = va,
 		.size = size,
 	};
@@ -260,6 +259,7 @@ gm_ret_t gmem_free(struct mm_struct *mm, unsigned long va, unsigned long size)
 		return -EBUSY;
 	}
 
+	req.peer_pid = proc->peer_pid;
 	ws = get_wait_station();
 	req.my_ws = ws->id;
 	ret = msg_send_nid(GMEM_FREE_VMA_REQUEST, proc->nid, proc->peer_nid,