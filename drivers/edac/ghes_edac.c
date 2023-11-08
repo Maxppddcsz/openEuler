1ac8e3172119578816f04b05257f053c0726af1c
Jia He <justin.he@arm.com>
2023-11-08 10:49:37 +0800

    EDAC/ghes: Add a notifier for reporting
    mainline inclusion
    from mainline-v6.2-rc1
    commit 8e40612f6146da1333e9bb5cfd9af7511c063d93
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8DQUX
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=8e40612f6146da1333e9bb5cfd9af7511c063d93
    
    ----------------------------------------------------------------------
    
     memory errors
    
    In order to make it a proper module and disentangle it from facilities,
    add a notifier for reporting memory errors. Use an atomic notifier
    because calls sites like ghes_proc_in_irq() run in interrupt context.
    
      [ bp: Massage commit message. ]
    
    Suggested-by: Borislav Petkov <bp@alien8.de>
    Signed-off-by: Jia He <justin.he@arm.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lore.kernel.org/r/20221010023559.69655-3-justin.he@arm.com
    Signed-off-by: Qian Zou <zouqian4@huawei.com>

--- drivers/edac/ghes_edac.c
+++ drivers/edac/ghes_edac.c
@@ -14,6 +14,7 @@
 #include <linux/dmi.h>
 #include "edac_module.h"
 #include <ras/ras_event.h>
+#include <linux/notifier.h>
 
 struct ghes_pvt {
 	struct mem_ctl_info *mci;
@@ -240,10 +241,13 @@ static void ghes_scan_system(void)
 	system_scanned = true;
 }
 
-void ghes_edac_report_mem_error(int sev, struct cper_sec_mem_err *mem_err)
+static int ghes_edac_report_mem_error(struct notifier_block *nb,
+				      unsigned long val, void *data)
 {
+	struct cper_sec_mem_err *mem_err = (struct cper_sec_mem_err *)data;
 	struct edac_raw_error_desc *e;
 	struct mem_ctl_info *mci;
+	unsigned long sev = val;
 	struct ghes_pvt *pvt;
 	unsigned long flags;
 	char *p;
@@ -254,7 +258,7 @@ void ghes_edac_report_mem_error(int sev, struct cper_sec_mem_err *mem_err)
 	 * know.
 	 */
 	if (WARN_ON_ONCE(in_nmi()))
-		return;
+		return NOTIFY_OK;
 
 	spin_lock_irqsave(&ghes_lock, flags);
 
@@ -500,7 +504,14 @@ void ghes_edac_report_mem_error(int sev, struct cper_sec_mem_err *mem_err)
 
 unlock:
 	spin_unlock_irqrestore(&ghes_lock, flags);
+
+	return NOTIFY_OK;
 }
+
+static struct notifier_block ghes_edac_mem_err_nb = {
+	.notifier_call	= ghes_edac_report_mem_error,
+	.priority	= 0,
+};
 
 /*
  * Known systems that are safe to enable this module.
@@ -629,6 +640,8 @@ int ghes_edac_register(struct ghes *ghes, struct device *dev)
 	ghes_pvt = pvt;
 	spin_unlock_irqrestore(&ghes_lock, flags);
 
+	ghes_register_report_chain(&ghes_edac_mem_err_nb);
+
 	/* only set on success */
 	refcount_set(&ghes_refcount, 1);
 
@@ -673,6 +686,8 @@ void ghes_edac_unregister(struct ghes *ghes)
 	mci = edac_mc_del_mc(mci->pdev);
 	if (mci)
 		edac_mc_free(mci);
+
+	ghes_unregister_report_chain(&ghes_edac_mem_err_nb);
 
 unlock:
 	mutex_unlock(&ghes_reg_mutex);
