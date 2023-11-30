8b4e42b9bbc28211a3ff7e91512003624c33d04e
wanghaibin <wanghaibin.wang@huawei.com>
2023-11-30 14:08:54 +0800

    KVM: arm64: GICv4.1: Inform the HiSilicon vtimer irqbypass capability
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8K89F
    CVE: NA
    
    ------------------------------------------------------------------
    
    HiSilicon implementation of GICv4.1 has provided direct injection capability
    of vtimer interrupts.  Inform users about the capability by vgic_global's
    has_direct_vtimer, which depends on the HW-capability from gic_kvm_info and
    kernel command "kvm-arm.vgic_v4_enable=1".
    
    Signed-off-by: wanghaibin <wanghaibin.wang@huawei.com>
    Signed-off-by: Zenghui Yu <yuzenghui@huawei.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Dongxu Sun <sundongxu3@huawei.com>

--- arch/arm64/kvm/vgic/vgic-v3.c
+++ arch/arm64/kvm/vgic/vgic-v3.c
@@ -655,6 +655,10 @@ int vgic_v3_probe(const struct gic_kvm_info *info)
 		kvm_info("GICv4%s support %sabled\n",
 			 kvm_vgic_global_state.has_gicv4_1 ? ".1" : "",
 			 gicv4_enable ? "en" : "dis");
+
+		kvm_vgic_global_state.has_direct_vtimer = info->has_vtimer && gicv4_enable;
+		kvm_info("vtimer-irqbypass support %sabled at GIC level\n",
+			 kvm_vgic_global_state.has_direct_vtimer ? "en" : "dis");
 	}
 
 	if (!info->vcpu.start) {
