da40228b410042a192d7d7e2d97197d6650beb37
Kunkun Jiang <jiangkunkun@huawei.com>
2023-11-30 14:08:49 +0800

    irqchip/gic-v4.1: Detect ITS vtimer interrupt bypass capability
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8K89F
    CVE: NA
    
    ------------------------------------------------------------------
    
    According to Hisilicon spec, using GITS_VERSION(0xC000) bit[12] to
    indicate ITS vtimer interrupt bypass capability.
    
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Dongxu Sun <sundongxu3@huawei.com>

--- drivers/irqchip/irq-gic-v3-its.c
+++ drivers/irqchip/irq-gic-v3-its.c
@@ -119,12 +119,19 @@ struct its_node {
 	int			numa_node;
 	unsigned int		msi_domain_flags;
 	u32			pre_its_base; /* for Socionext Synquacer */
+	/**
+	 * Hisilicon implement reg used for indicating
+	 * direct vPPI injection capability.
+	 */
+	u32			version;
 	int			vlpi_redist_offset;
 };
 
-#define is_v4(its)		(!!((its)->typer & GITS_TYPER_VLPIS))
-#define is_v4_1(its)		(!!((its)->typer & GITS_TYPER_VMAPP))
-#define device_ids(its)		(FIELD_GET(GITS_TYPER_DEVBITS, (its)->typer) + 1)
+#define is_v4(its)			(!!((its)->typer & GITS_TYPER_VLPIS))
+#define is_v4_1(its)			(!!((its)->typer & GITS_TYPER_VMAPP))
+#define device_ids(its)			(FIELD_GET(GITS_TYPER_DEVBITS, (its)->typer) + 1)
+
+#define is_vtimer_irqbypass(its)	(!!((its)->version & GITS_VERSION_VTIMER))
 
 #define ITS_ITT_ALIGN		SZ_256
 
@@ -5292,6 +5299,7 @@ static int __init its_probe_one(struct resource *res,
 	INIT_LIST_HEAD(&its->its_device_list);
 	typer = gic_read_typer(its_base + GITS_TYPER);
 	its->typer = typer;
+	its->version = readl_relaxed(its_base + GITS_VERSION);
 	its->base = its_base;
 	its->phys_base = res->start;
 	if (is_v4(its)) {
