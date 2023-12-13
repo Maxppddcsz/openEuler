f1768521f92e873812722619085cebf8d0c70233
Lu Jialin <lujialin4@huawei.com>
2023-12-13 03:23:44 +0000

    memcg: Export memcg.high from cgroupv2 to cgroupv1
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8JVN0
    
    --------------------------------
    
    Export memory.high from cgroupv2 to cgroupv1. Therefore, when the usage
    of the memcg is larger than memory.high, some pages will be reclaimed
    before return to userland, which will throttle the process.
    
    Only export memory.high number in mem_cgroup_legacy_files and move
    related functions in front of mem_cgroup_legacy_files. There is no need
    to other changes.
    
    Signed-off-by: Lu Jialin <lujialin4@huawei.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5019,6 +5019,9 @@ static ssize_t memory_min_write(struct kernfs_open_file *of,
 static int memory_low_show(struct seq_file *m, void *v);
 static ssize_t memory_low_write(struct kernfs_open_file *of,
 				char *buf, size_t nbytes, loff_t off);
+static int memory_high_show(struct seq_file *m, void *v);
+static ssize_t memory_high_write(struct kernfs_open_file *of,
+				 char *buf, size_t nbytes, loff_t off);
 #endif
 
 static struct cftype mem_cgroup_legacy_files[] = {
@@ -5159,6 +5162,12 @@ static struct cftype mem_cgroup_legacy_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.seq_show = memory_low_show,
 		.write = memory_low_write,
+	},
+	{
+		.name = "high",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = memory_high_show,
+		.write = memory_high_write,
 	},
 #endif
 	{ },	/* terminate */
