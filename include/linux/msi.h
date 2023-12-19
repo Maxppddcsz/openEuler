3e1173101226e0a83440b5b7aa41c83b65075ce9
Lin Ruizhe <linruizhe@huawei.com>
2023-12-19 21:06:35 +0800

    msi: Add kabi_reserve in msi.h
    hulk inclusion
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8PS7G
    CVE: NA
    
    ---------------------------
    
    Add KABI_RESERVE in msi.h
    
    conflict:
    	include/linux/msi.h.rej
    
    Signed-off-by: Lin Yujun <linyujun809@huawei.com>

--- include/linux/msi.h
+++ include/linux/msi.h
@@ -26,6 +26,7 @@
 #include <linux/list.h>
 #include <linux/irq.h>
 #include <linux/bits.h>
+#include <linux/kabi.h>
 
 #include <asm/msi.h>
 
@@ -203,6 +204,8 @@ struct msi_desc {
 		struct pci_msi_desc	pci;
 		struct msi_desc_data	data;
 	};
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
 };
 
 /*
