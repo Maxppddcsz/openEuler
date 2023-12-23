09b79c4f782b62516ec2781647821ae36851f7b8
Li Zhengyu <lizhengyu3@huawei.com>
2023-12-23 12:10:53 +0000

    arm64: Add non nmi ipi backtrace support
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I59IQS
    CVE: NA
    
    --------------------------------
    
    Use non nmi ipi to support backtrace on arm64 with nmi
    unsupported.
    It has been tested on qemu.
    
    Signed-off-by: Li Zhengyu <lizhengyu3@huawei.com>
    Reviewed-by: Liao Chang <liaochang1@huawei.com>
    Reviewed-by: Wei Li <liwei391@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Liao Chen <liaochen4@huawei.com>

--- arch/arm64/kernel/ipi_nmi.c
+++ arch/arm64/kernel/ipi_nmi.c
@@ -33,13 +33,24 @@ void arm64_send_nmi(cpumask_t *mask)
 	__ipi_send_mask(ipi_nmi_desc, mask);
 }
 
-bool arch_trigger_cpumask_backtrace(const cpumask_t *mask, bool exclude_self)
+static void ipi_cpu_backtrace(void *info)
+{
+	__printk_safe_enter();
+	nmi_cpu_backtrace(get_irq_regs());
+	__printk_safe_exit();
+}
+
+static void arm64_send_ipi(cpumask_t *mask)
+{
+	smp_call_function_many(mask, ipi_cpu_backtrace, NULL, false);
+}
+
+bool arch_trigger_cpumask_backtrace(const cpumask_t *mask, int exclude_cpus)
 {
 	if (!ipi_nmi_desc)
-		return false;
-
-	nmi_trigger_cpumask_backtrace(mask, exclude_self, arm64_send_nmi);
-
+		nmi_trigger_cpumask_backtrace(mask, exclude_cpus, arm64_send_ipi);
+	else
+		nmi_trigger_cpumask_backtrace(mask, exclude_cpus, arm64_send_nmi);
 	return true;
 }
 
