torvalds/linux

commit ffde703470b03b1000017ed35c4f90a90caa22cf

arm64: Enable seccomp architecture tracking
To enable seccomp constant action bitmaps, we need to have a static
mapping to the audit architecture and system call table size. Add these
for arm64.

Signed-off-by: Kees Cook <keescook@chromium.org>

arch/arm64/include/asm/seccomp.h
@@ -19,4 +19,13 @@
 
 #include <asm-generic/seccomp.h>
 
+#define SECCOMP_ARCH_NATIVE		AUDIT_ARCH_AARCH64
+#define SECCOMP_ARCH_NATIVE_NR		NR_syscalls
+#define SECCOMP_ARCH_NATIVE_NAME	"aarch64"
+#ifdef CONFIG_COMPAT
+# define SECCOMP_ARCH_COMPAT		AUDIT_ARCH_ARM
+# define SECCOMP_ARCH_COMPAT_NR	__NR_compat_syscalls
+# define SECCOMP_ARCH_COMPAT_NAME	"arm"
+#endif
+
 #endif /* _ASM_SECCOMP_H */

Maxppddcsz/openEuler

commit 99fb8718ff2c0ba671f1e032abea304963c50b6b

arm64: seccomp: fix compilation error with ILP32 support
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I8S6IA

-------------------------------------------------

When ILP32 support is enabled, compilation on ARM64 would complain
about an undeclared __NR_compat_syscalls, since commit 0b18db62881c
("arm64: ilp32: introduce syscall table for ILP32") changes the config
that __NR_compat_syscalls depends on. Modify the config for
SECCOMP_ARCH_COMPAT_NR accordingly.

Fixes: ffde703470b0 ("arm64: Enable seccomp architecture tracking")
Signed-off-by: GONG, Ruiqi <gongruiqi1@huawei.com>
Signed-off-by: Yi Yang <yiyang13@huawei.com>

arch/arm64/include/asm/seccomp.h
@@ -52,10 +52,10 @@ static inline const int *get_compat_mode1_syscalls(void)
 #define SECCOMP_ARCH_NATIVE		AUDIT_ARCH_AARCH64
 #define SECCOMP_ARCH_NATIVE_NR		NR_syscalls
 #define SECCOMP_ARCH_NATIVE_NAME	"aarch64"
-#ifdef CONFIG_COMPAT
+#ifdef CONFIG_AARCH32_EL0
 # define SECCOMP_ARCH_COMPAT		AUDIT_ARCH_ARM
 # define SECCOMP_ARCH_COMPAT_NR	__NR_compat_syscalls
 # define SECCOMP_ARCH_COMPAT_NAME	"arm"
-#endif
+#endif /* CONFIG_AARCH32_EL0 */
 
 #endif /* _ASM_SECCOMP_H */