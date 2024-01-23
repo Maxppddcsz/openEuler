8365e8a2a29efd03e010ff302461dc400143bc0c
Chen Ridong <chenridong@huawei.com>
2024-01-23 12:18:02 +0000

    cgrou/misc: misc for controlling fd
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8YC6O
    
    ----------------------------------------------------------------------
    
    Filescgroup is used to control fd in cgroup v1, while in cgroup v2,
    it is better to control fd with misc, which provides the resource
    limiting and tracking mechanism for the scalar resources.
    Filescontrol.c was renamed to files-cgroup.c, filescontrol.h
    contains interfaces to outside, which decides using filescgroup or
    misc with config.
    
    Signed-off-by: Chen Ridong <chenridong@huawei.com>

--- include/linux/misc_cgroup.h
+++ include/linux/misc_cgroup.h
@@ -18,6 +18,10 @@ enum misc_res_type {
 	/* AMD SEV-ES ASIDs resource */
 	MISC_CG_RES_SEV_ES,
 #endif
+#ifdef CONFIG_CGROUP_MISC_FD
+	/* fd for file control  */
+	MISC_FD,
+#endif
 	MISC_CG_RES_TYPES
 };
 
