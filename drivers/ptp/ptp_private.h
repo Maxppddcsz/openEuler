9b9aba0e5e710a342cec664274aa381a563586a0
Yang Yingliang <yangyingliang@huawei.com>
2024-01-26 11:04:47 +0800

    kabi: reserve space for struct ptp_clock
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8Z2NU
    CVE: NA
    
    -------------------------------
    
    reserve space for struct ptp_clock.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>

--- drivers/ptp/ptp_private.h
+++ drivers/ptp/ptp_private.h
@@ -53,6 +53,10 @@ struct ptp_clock {
 	struct mutex n_vclocks_mux; /* protect concurrent n_vclocks access */
 	bool is_virtual_clock;
 	bool has_cycles;
+	KABI_RESERVE(1)
+	KABI_RESERVE(2)
+	KABI_RESERVE(3)
+	KABI_RESERVE(4)
 };
 
 #define info_to_vclock(d) container_of((d), struct ptp_vclock, info)
