057b5d86d50e8bcd970389024df6a43f4eeb8168
Chen Ridong <chenridong@huawei.com>
2024-01-27 10:50:31 +0000

    misc: add MISC_CG_RES_FD resource to control fd.
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8OIQR
    
    ----------------------------------------------------------------------
    
    Filescgroup is used to control fd in cgroup v1 as cgroup controller,
    which will not be merged to mainline. It is better to control fd with misc,
    which provides the resource limiting and tracking mechanism for the scalar
    resources and already be merged, so add MISC_CG_RES_FD resource to control
    fd. Filescgroup will be abandoned in the future.
    
    Signed-off-by: Chen Ridong <chenridong@huawei.com>

--- fs/filescontrol.c
+++ fs/filescontrol.c
@@ -8,6 +8,14 @@
 #include <linux/fdtable.h>
 #include <linux/filescontrol.h>
 #include <linux/files-cgroup.h>
+#include <linux/misc-fd.h>
+
+struct static_key_false misc_fd_enable_key;
+
+static inline bool file_cg_misc_enabled(void)
+{
+	return static_branch_likely(&misc_fd_enable_key);
+}
 
 u64 file_cg_count_fds(struct files_struct *files)
 {
@@ -23,32 +31,58 @@ u64 file_cg_count_fds(struct files_struct *files)
 
 int files_cg_alloc_fd(struct files_struct *files, u64 n)
 {
+	if (file_cg_misc_enabled())
+		return misc_fd_alloc_fd(files, n);
+
 	return files_cgroup_alloc_fd(files, n);
 }
 
 void files_cg_unalloc_fd(struct files_struct *files, u64 n)
 {
+	if (file_cg_misc_enabled())
+		return misc_fd_unalloc_fd(files, n);
+
 	return files_cgroup_unalloc_fd(files, n);
 }
 
 void files_cg_assign(struct files_struct *files)
 {
+	if (file_cg_misc_enabled())
+		return misc_fd_assign(files);
+
 	return files_cgroup_assign(files);
 }
 
 void files_cg_remove(struct files_struct *files)
 {
-	return files_cgroup_assign(files);
+	if (file_cg_misc_enabled())
+		return misc_fd_remove(files);
+
+	return files_cgroup_remove(files);
 }
 
 int files_cg_dup_fds(struct files_struct *newf)
 {
+	if (file_cg_misc_enabled())
+		return misc_fd_dup_fds(newf);
+
 	return files_cgroup_dup_fds(newf);
 }
 
 void files_cg_put_fd(struct files_struct *files, unsigned int fd)
 {
+	if (file_cg_misc_enabled())
+		return misc_fd_put_fd(files, fd);
+
 	return files_cgroup_put_fd(files, fd);
 }
 
+static int __init enable_misc_fd(char *s)
+{
+	static_branch_enable(&misc_fd_enable_key);
+	pr_info("file_cg  enable misc to control fd\n");
+
+	return 1;
+}
+__setup("file_cg=misc", enable_misc_fd);
 
