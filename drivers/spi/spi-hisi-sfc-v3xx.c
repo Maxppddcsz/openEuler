3e2ff352870322a8c81641931630d23b5bd3ceb2
slim6882 <yangjunshuo@huawei.com>
2024-01-29 20:13:19 +0800

    spi: hisi-sfc-v3xx: Return IRQ_NONE if no interrupts were detected
    	modified:   drivers/spi/spi-hisi-sfc-v3xx.c
    
    	modified:   drivers/spi/spi-hisi-sfc-v3xx.c

--- drivers/spi/spi-hisi-sfc-v3xx.c
+++ drivers/spi/spi-hisi-sfc-v3xx.c
@@ -378,6 +378,12 @@ static irqreturn_t hisi_sfc_v3xx_isr(int irq, void *data)
 {
 	struct hisi_sfc_v3xx_host *host = data;
 
+	u32 reg;
+
+	reg = readl(host->regbase + HISI_SFC_V3XX_INT_STAT);
+	if (!reg) 
+		return IRQ_NONE;
+
 	hisi_sfc_v3xx_disable_int(host);
 
 	complete(host->completion);
