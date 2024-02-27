2b3b575a7519782b67b4bca61bd29fbd014b75db
xiongzhou4 <xiongzhou4@huawei.com>
2024-02-27 15:34:42 +0800

    GCOV: Add value profile support for kernel.
    GCC inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I93ZKQ
    
    ---------------------------------
    
    Port from openEuler 22.03 LTS SP2:
    https://gitee.com/openeuler/kernel/pulls/773
    
    This feature add value profile support for kernel by changing GCOV
    option "-fprofile-arcs" to "-fprofile-generate" when the new added
    config "PGO_KERNEL" is set to y.
    
    Like GCOV, the symbols required by value profile are migrated from
    GCC source codes as they cannot be linked to kernel. Specifically,
    from libgcc/libgcov-profiler.c and libgcc/libgcov.h to
    kernel/gcov/gcc_base.c.
    
    kernel options:
    CONFIG_PGO_KERNEL=y
    
    Signed-off-by: xiongzhou4 <xiongzhou4@huawei.com>

--- kernel/gcov/gcc_base.c
+++ kernel/gcov/gcc_base.c
@@ -84,3 +84,174 @@ void __gcov_exit(void)
 	/* Unused. */
 }
 EXPORT_SYMBOL(__gcov_exit);
+
+#ifdef CONFIG_PGO_KERNEL
+/* Maximum number of tracked TOP N value profiles.  */
+#define GCOV_TOPN_MAXIMUM_TRACKED_VALUES 32
+
+void __gcov_merge_topn(gcov_type *counters, unsigned int n_counters)
+{
+	/* Unused. */
+}
+EXPORT_SYMBOL(__gcov_merge_topn);
+
+struct indirect_call_tuple {
+	void *callee;
+
+	gcov_type *counters;
+};
+
+/* Kernel does not support __thread keyword. */
+struct indirect_call_tuple __gcov_indirect_call;
+EXPORT_SYMBOL(__gcov_indirect_call);
+
+gcov_type __gcov_time_profiler_counter;
+EXPORT_SYMBOL(__gcov_time_profiler_counter);
+
+/* GCOV key-value pair linked list type.  */
+struct gcov_kvp
+{
+	gcov_type value;
+	gcov_type count;
+	struct gcov_kvp *next;
+};
+
+/*
+ * Tries to determine N most commons value among its inputs.
+ */
+static inline void __gcov_topn_values_profiler_body(gcov_type *counters,
+		gcov_type value)
+{
+	gcov_type count = 1;
+
+	/* In the multi-threaded mode, we can have an already merged profile
+	with a negative total value.  In that case, we should bail out.  */
+	if (counters[0] < 0)
+		return;
+	counters[0]++;
+
+	struct gcov_kvp *prev_node = NULL;
+	struct gcov_kvp *minimal_node = NULL;
+	struct gcov_kvp *current_node = (struct gcov_kvp *)(intptr_t)counters[2];
+
+	while (current_node) {
+		if (current_node->value == value) {
+			current_node->count += count;
+			return;
+		}
+
+		if (minimal_node == NULL
+		|| current_node->count < minimal_node->count)
+			minimal_node = current_node;
+
+		prev_node = current_node;
+		current_node = current_node->next;
+	}
+
+	if (counters[1] == GCOV_TOPN_MAXIMUM_TRACKED_VALUES) {
+		if (--minimal_node->count < count) {
+			minimal_node->value = value;
+			minimal_node->count = count;
+		}
+
+		return;
+	} else {
+		struct gcov_kvp *new_node = (struct gcov_kvp *)kcalloc(1, sizeof(struct gcov_kvp), GFP_KERNEL);
+		// The original gcc code uses calloc.
+		// struct gcov_kvp *new_node = (struct gcov_kvp *)calloc(1, sizeof(struct gcov_kvp));
+		// struct gcov_kvp *new_node = allocate_gcov_kvp();
+		if (new_node == NULL)
+			return;
+
+		new_node->value = value;
+		new_node->count = count;
+
+		int success = 0;
+		if (!counters[2]) {
+			counters[2] = (intptr_t)new_node;
+			success = 1;
+		} else if (prev_node && !prev_node->next) {
+			prev_node->next = new_node;
+			success = 1;
+		}
+
+		/* Increment number of nodes.  */
+		if (success)
+			counters[1]++;
+	}
+}
+
+void __gcov_topn_values_profiler(gcov_type *counters, gcov_type value)
+{
+	__gcov_topn_values_profiler_body(counters, value);
+}
+EXPORT_SYMBOL(__gcov_topn_values_profiler);
+
+/*
+ * Tries to determine the most common value among its inputs.
+ */
+static inline void __gcov_indirect_call_profiler_body(gcov_type value,
+		void *cur_func)
+{
+	/* Removed the C++ virtual tables contents as kernel is written in C. */
+	if (cur_func == __gcov_indirect_call.callee)
+		__gcov_topn_values_profiler_body(__gcov_indirect_call.counters, value);
+
+	__gcov_indirect_call.callee = NULL;
+}
+
+void __gcov_indirect_call_profiler_v4(gcov_type value, void *cur_func)
+{
+	__gcov_indirect_call_profiler_body(value, cur_func);
+}
+EXPORT_SYMBOL(__gcov_indirect_call_profiler_v4);
+
+/*
+ * Increase corresponding COUNTER by VALUE.
+ */
+void __gcov_average_profiler(gcov_type *counters, gcov_type value)
+{
+	counters[0] += value;
+	counters[1]++;
+}
+EXPORT_SYMBOL(__gcov_average_profiler);
+
+void __gcov_ior_profiler(gcov_type *counters, gcov_type value)
+{
+	*counters |= value;
+}
+EXPORT_SYMBOL(__gcov_ior_profiler);
+
+/*
+ * If VALUE is a power of two, COUNTERS[1] is incremented.	Otherwise
+ * COUNTERS[0] is incremented.
+ */
+void __gcov_pow2_profiler(gcov_type *counters, gcov_type value)
+{
+	if (value == 0 || (value & (value - 1)))
+		counters[0]++;
+	else
+		counters[1]++;
+}
+EXPORT_SYMBOL(__gcov_pow2_profiler);
+
+/*
+ * If VALUE is in interval <START, START + STEPS - 1>, then increases the
+ * corresponding counter in COUNTERS.	If the VALUE is above or below
+ * the interval, COUNTERS[STEPS] or COUNTERS[STEPS + 1] is increased
+ * instead.
+ */
+void __gcov_interval_profiler(gcov_type *counters, gcov_type value,
+		int start, unsigned int steps)
+{
+	gcov_type delta = value - start;
+
+	if (delta < 0)
+		counters[steps + 1]++;
+	else if (delta >= steps)
+		counters[steps]++;
+	else
+		counters[delta]++;
+}
+EXPORT_SYMBOL(__gcov_interval_profiler);
+#endif
