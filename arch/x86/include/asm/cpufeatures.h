877c6dc9c471a1f79c91758fcf83f46ea2efd63a
Borislav Petkov (AMD) <bp@alien8.de>
2024-03-18 20:17:57 +0800

    x86/srso: Add a Speculative RAS Overflow mitigation
    stable inclusion
    from stable-v5.10.189
    commit 3f9b7101bea1dcb63410c016ceb266f6e9f733c9
    category: bugfix
    bugzilla: https://gitee.com/src-openeuler/kernel/issues/I7RQ67
    CVE: CVE-2023-20569
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=3f9b7101bea1dcb63410c016ceb266f6e9f733c9
    
    --------------------------------
    
    Upstream commit: fb3bd914b3ec28f5fb697ac55c4846ac2d542855
    
    Add a mitigation for the speculative return address stack overflow
    vulnerability found on AMD processors.
    
    The mitigation works by ensuring all RET instructions speculate to
    a controlled location, similar to how speculation is controlled in the
    retpoline sequence.  To accomplish this, the __x86_return_thunk forces
    the CPU to mispredict every function return using a 'safe return'
    sequence.
    
    To ensure the safety of this mitigation, the kernel must ensure that the
    safe return sequence is itself free from attacker interference.  In Zen3
    and Zen4, this is accomplished by creating a BTB alias between the
    untraining function srso_untrain_ret_alias() and the safe return
    function srso_safe_ret_alias() which results in evicting a potentially
    poisoned BTB entry and using that safe one for all function returns.
    
    In older Zen1 and Zen2, this is accomplished using a reinterpretation
    technique similar to Retbleed one: srso_untrain_ret() and
    srso_safe_ret().
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
     Conflicts:
    	arch/x86/include/asm/processor.h
    	arch/x86/kernel/cpu/bugs.c
    	arch/x86/kernel/cpu/common.c
    	include/linux/cpu.h
    
    Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>
    (cherry picked from commit 8105f71bb9145778d57008e312634c780c9180cb)
    Signed-off-by: Jialin Zhang <zhangjialin11@huawei.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -323,6 +323,9 @@
 #define X86_FEATURE_USE_IBPB_FW	       (11*32+16) /* "" Use IBPB during runtime firmware calls */
 #define X86_FEATURE_RSB_VMEXIT_LITE		(11*32+17) /* "" Fill RSB on VM exit when EIBRS is enabled */
 
+#define X86_FEATURE_SRSO		(11*32+24) /* "" AMD BTB untrain RETs */
+#define X86_FEATURE_SRSO_ALIAS		(11*32+25) /* "" AMD BTB untrain RETs through aliasing */
+
 /* Intel-defined CPU features, CPUID level 0x00000007:1 (EAX), word 12 */
 #define X86_FEATURE_AVX_VNNI		(12*32+ 4) /* AVX VNNI instructions */
 #define X86_FEATURE_AVX512_BF16		(12*32+ 5) /* AVX512 BFLOAT16 instructions */
@@ -475,4 +478,6 @@
 #define X86_BUG_GDS			X86_BUG(30) /* CPU is affected by Gather Data Sampling */
 #define X86_BUG_DIV0			X86_BUG(31) /* AMD DIV0 speculation bug */
 
+/* BUG word 2 */
+#define X86_BUG_SRSO			X86_BUG(1*32 + 0) /* AMD SRSO bug */
 #endif /* _ASM_X86_CPUFEATURES_H */
