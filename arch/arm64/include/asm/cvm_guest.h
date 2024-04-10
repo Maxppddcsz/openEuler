9699b5fe41bcbfe3326c03e6ccec7fe09a75cc46
Jingxian He <hejingxian@huawei.com>
2024-04-10 13:14:12 +0800

    dma: add bounce buffer feature for cvm guest
    hulk inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9CC0X
    CVE: N/A
    
    Reference: N/A
    
    --------------------------------
    
    Add bounce buffer feature for cvm guest os:
    1) Cvm guest mapped memory is secure memory.
    2) Qemu/kvm cannot access the secure memory.
    3) Use bounce buffer as memory shared by cvm guest and qemu/kvm.
    
    Signed-off-by: Jingxian He <hejingxian@huawei.com>

---
+++ arch/arm64/include/asm/cvm_guest.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+ */
+#ifndef __ASM_CVM_GUEST_H
+#define __ASM_CVM_GUEST_H
+
+#ifdef CONFIG_CVM_GUEST
+static inline bool cvm_mem_encrypt_active(void)
+{
+	return false;
+}
+
+int set_cvm_memory_encrypted(unsigned long addr, int numpages);
+
+int set_cvm_memory_decrypted(unsigned long addr, int numpages);
+
+bool is_cvm_world(void);
+
+#endif
+#endif
