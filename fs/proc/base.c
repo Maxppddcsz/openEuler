openeuler-mirror/kernel

commit 34239429b1253e3d19cebf15277f46916519b1c2

sched: programmable: Add user interface of task tag
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I8OIT1

--------------------------------

Add user interface of task tag, bridges the information
gap between user-mode and kernel mode.

Add proc interface:
  /proc/${pid}/task/${pid}/tag

Signed-off-by: Chen Hui <judy.chenhui@huawei.com>
Signed-off-by: Ren Zhijie <renzhijie2@huawei.com>
Signed-off-by: Hui Tang <tanghui20@huawei.com>
Signed-off-by: Guan Jing <guanjing6@huawei.com>

fs/proc/base.c
@@ -3672,6 +3672,64 @@ static const struct inode_operations proc_tid_comm_inode_operations = {
 		.permission	= proc_tid_comm_permission,
 };
 
+#ifdef CONFIG_BPF_SCHED
+static ssize_t pid_tag_write(struct file *file, const char __user *buf,
+				size_t count, loff_t *offset)
+{
+	struct inode *inode = file_inode(file);
+	struct task_struct *tsk;
+	int err = 0;
+	long tag = 0;
+
+	tsk = get_proc_task(inode);
+	if (!tsk)
+		return -ESRCH;
+
+	if (unlikely(tsk->pid == 1)) {
+		err = -EPERM;
+		goto out;
+	}
+
+	err = kstrtol_from_user(buf, count, 0, &tag);
+	if (err)
+		goto out;
+
+	sched_settag(tsk, tag);
+
+out:
+	put_task_struct(tsk);
+	return err < 0 ? err : count;
+}
+
+static int pid_tag_show(struct seq_file *m, void *v)
+{
+	struct inode *inode = m->private;
+	struct task_struct *tsk;
+
+	tsk = get_proc_task(inode);
+	if (!tsk)
+		return -ESRCH;
+
+	seq_printf(m, "%ld\n", tsk->tag);
+	put_task_struct(tsk);
+
+	return 0;
+}
+
+static int pid_tag_open(struct inode *inode, struct file *flip)
+{
+	return single_open(flip, pid_tag_show, inode);
+}
+
+static const struct file_operations proc_pid_tag_operations = {
+		.open		= pid_tag_open,
+		.read		= seq_read,
+		.write		= pid_tag_write,
+		.llseek		= seq_lseek,
+		.release	= single_release,
+};
+#endif
+
 /*
  * Tasks
  */
@@ -3780,6 +3838,9 @@ static const struct pid_entry tid_base_stuff[] = {
 #endif
 #ifdef CONFIG_QOS_SCHED_DYNAMIC_AFFINITY
 	REG("preferred_cpuset", 0644, proc_preferred_cpuset_operations),
+#endif
+#ifdef CONFIG_BPF_SCHED
+	REG("tag", 0644, proc_pid_tag_operations),
 #endif
 };
 

Maxppddcsz/openEuler

commit 2b575efdecda3a59794e1231b795df2fee847260

sched: programmable: Allow set tag for pid 1.
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I9NQM5
CVE: NA

--------------------------------

Fixes: 34239429b125 ("sched: programmable: Add user interface of task tag")
Signed-off-by: Hui Tang <tanghui20@huawei.com>

fs/proc/base.c
@@ -3773,11 +3773,6 @@ static ssize_t pid_tag_write(struct file *file, const char __user *buf,
 	if (!tsk)
 		return -ESRCH;
 
-	if (unlikely(tsk->pid == 1)) {
-		err = -EPERM;
-		goto out;
-	}
-
 	err = kstrtol_from_user(buf, count, 0, &tag);
 	if (err)
 		goto out;