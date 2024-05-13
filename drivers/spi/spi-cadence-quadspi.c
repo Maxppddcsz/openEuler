290c871f18354725d65c0af0f35d857ede841acf
Vaishnav Achath <vaishnav.a@ti.com>
2024-05-13 17:22:45 +0800

    spi: cadence-quadspi: Handle spi_unregister_master() in remove()
    mainline inclusion
    from mainline-v5.19-rc1
    commit 606e5d408184989f53028125e0cb5aa6713362d5
    category: bugfix
    bugzilla: 189766
    CVE: CVE-2024-26807
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=606e5d408184989f53028125e0cb5aa6713362d5
    
    --------------------------------
    
    Currently devres managed removal of the spi_controller happens after
    removing the power domain of the host platform_device.While this
    does not affect the clean removal of the controller, but affects
    graceful removal of the child devices if the child  device removal
    requires issuing commands over SPI.
    
    Eg. flash device being soft reset to 1S-1S-1S mode before removal
    so that on next probe operations in 1S-1S-1S mode is successful.
    
    Failure is seen when `rmmod spi-cadence-quadspi` is performed:
    
    root@j7-evm:~# rmmod spi_cadence_quadspi
    [ 49.230996] cadence-qspi 47050000.spi: QSPI is still busy after 500ms timeout.
    [ 49.238209] spi-nor spi1.0: operation failed with -110
    [ 49.244457] spi-nor spi1.0: Software reset failed: -110
    
    and on subsequent modprobe the OSPI flash probe fails as it
    is in 8D-8D-8D mode since the previous soft reset did not happen.
    
    root@j7-evm:~# modprobe spi_cadence_quadspi
    [ 73.253536] spi-nor spi0.0: unrecognized JEDEC id bytes: ff ff ff ff ff ff
    [ 73.260476] spi-nor: probe of spi0.0 failed with error -2
    
    This commit adds necessary changes to perform spi_unregister_master()
    in the host device remove() so that the child devices are gracefully
    removed before the power domain is removed.
    
    changes tested on J721E with mt35xu512aba flash.
    
    Signed-off-by: Vaishnav Achath <vaishnav.a@ti.com>
    Link: https://lore.kernel.org/r/20220511115516.14894-1-vaishnav.a@ti.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Liao Chen <liaochen4@huawei.com>

--- drivers/spi/spi-cadence-quadspi.c
+++ drivers/spi/spi-cadence-quadspi.c
@@ -57,7 +57,7 @@ struct cqspi_flash_pdata {
 
 struct cqspi_st {
 	struct platform_device	*pdev;
-
+	struct spi_master	*master;
 	struct clk		*clk;
 	unsigned int		sclk;
 
@@ -1186,7 +1186,7 @@ static int cqspi_probe(struct platform_device *pdev)
 	int ret;
 	int irq;
 
-	master = spi_alloc_master(&pdev->dev, sizeof(*cqspi));
+	master = devm_spi_alloc_master(&pdev->dev, sizeof(*cqspi));
 	if (!master) {
 		dev_err(&pdev->dev, "spi_alloc_master failed\n");
 		return -ENOMEM;
@@ -1198,6 +1198,7 @@ static int cqspi_probe(struct platform_device *pdev)
 	cqspi = spi_master_get_devdata(master);
 
 	cqspi->pdev = pdev;
+	cqspi->master = master;
 	platform_set_drvdata(pdev, cqspi);
 
 	/* Obtain configuration from OF. */
@@ -1315,7 +1316,7 @@ static int cqspi_probe(struct platform_device *pdev)
 			goto probe_setup_failed;
 	}
 
-	ret = devm_spi_register_master(dev, master);
+	ret = spi_register_master(master);
 	if (ret) {
 		dev_err(&pdev->dev, "failed to register SPI ctlr %d\n", ret);
 		goto probe_setup_failed;
@@ -1338,6 +1339,7 @@ static int cqspi_remove(struct platform_device *pdev)
 {
 	struct cqspi_st *cqspi = platform_get_drvdata(pdev);
 
+	spi_unregister_master(cqspi->master);
 	cqspi_controller_enable(cqspi, 0);
 
 	if (cqspi->rx_chan)
