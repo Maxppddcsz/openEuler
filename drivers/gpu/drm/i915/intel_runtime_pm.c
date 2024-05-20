8451cc8d1c8721674a272ac1fadff4a3e5d0ab0e
Ville Syrjälä <ville.syrjala@linux.intel.com>
2024-05-20 20:31:26 +0800

    drm/i915: Fix oops due to missing stack depot
    mainline inclusion
    from mainline-v5.17-rc4
    commit eb48d42198792f1330bbb3e82ac725d43c13fe02
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9GSSR
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=eb48d42198792f1330bbb3e82ac725d43c13fe02
    
    --------------------------------
    
    We call __save_depot_stack() unconditionally so the stack depot
    must always be initialized or else we'll oops on platforms without
    runtime pm support.
    
    Presumably we've not seen this in CI due to stack_depot_init()
    already getting called via drm_mm_init()+CONFIG_DRM_DEBUG_MM.
    
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Marco Elver <elver@google.com> # stackdepot
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Imre Deak <imre.deak@intel.com>
    Fixes: 2dba5eb1c73b ("lib/stackdepot: allow optional init and stack_table allocation by kvmalloc()")
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220126081539.23227-1-ville.syrjala@linux.intel.com
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Imre Deak <imre.deak@intel.com>
    (cherry picked from commit 751a9d69b19702af35b0fedfb8ff362027c1cf0c)
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -78,9 +78,7 @@ static void __print_depot_stack(depot_stack_handle_t stack,
 static void init_intel_runtime_pm_wakeref(struct intel_runtime_pm *rpm)
 {
 	spin_lock_init(&rpm->debug.lock);
-
-	if (rpm->available)
-		stack_depot_init();
+	stack_depot_init();
 }
 
 static noinline depot_stack_handle_t
