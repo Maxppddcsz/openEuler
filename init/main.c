21f9766060fece837dd80a31cece04df8cc992c0
Vijayanand Jitta <vjitta@codeaurora.org>
2024-05-20 20:31:22 +0800

    lib: stackdepot: add support to disable stack depot
    mainline inclusion
    from mainline-v5.12-rc1
    commit e1fdc403349c64fa58f4c163f4bf9b860b4db808
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9GSSR
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e1fdc403349c64fa58f4c163f4bf9b860b4db808
    
    --------------------------------
    
    Add a kernel parameter stack_depot_disable to disable stack depot.  So
    that stack hash table doesn't consume any memory when stack depot is
    disabled.
    
    The use case is CONFIG_PAGE_OWNER without page_owner=on.  Without this
    patch, stackdepot will consume the memory for the hashtable.  By default,
    it's 8M which is never trivial.
    
    With this option, in CONFIG_PAGE_OWNER configured system, page_owner=off,
    stack_depot_disable in kernel command line, we could save the wasted
    memory for the hashtable.
    
    [akpm@linux-foundation.org: fix CONFIG_STACKDEPOT=n build]
    
    Link: https://lkml.kernel.org/r/1611749198-24316-2-git-send-email-vjitta@codeaurora.org
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Vijayanand Jitta <vjitta@codeaurora.org>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Yogesh Lal <ylal@codeaurora.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	Documentation/admin-guide/kernel-parameters.txt
    	include/linux/stackdepot.h
    	init/main.c
    	lib/stackdepot.c
    [context conflicts]
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>

--- init/main.c
+++ init/main.c
@@ -98,6 +98,7 @@
 #include <linux/jump_label.h>
 #include <linux/kcsan.h>
 #include <linux/init_syscalls.h>
+#include <linux/stackdepot.h>
 #include <linux/randomize_kstack.h>
 
 #include <asm/io.h>
@@ -829,6 +830,7 @@ static void __init mm_init(void)
 	init_debug_pagealloc();
 	kfence_alloc_pool();
 	report_meminit();
+	stack_depot_init();
 	mem_init();
 	/* page_owner must be initialized after buddy is ready */
 	page_ext_init_flatmem_late();
