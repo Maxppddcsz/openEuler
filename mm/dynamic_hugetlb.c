bf679717836099b7380d94afee4ea52ee771c834
Liu Shixin <liushixin2@huawei.com>
2024-05-27 15:40:36 +0800

    mm/dynamic_hugetlb: check NULL pointer for early parameter
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9K8D1
    CVE: NA
    
    --------------------------------
    
    Add null pointer check for early parameter.
    
    Fixes: a8a836a36072 ("mm/dynamic_hugetlb: establish the dynamic hugetlb feature framework")
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    (cherry picked from commit ab9150fefa98f1c43de1845791ef4b11008180dd)

--- mm/dynamic_hugetlb.c
+++ mm/dynamic_hugetlb.c
@@ -1261,6 +1261,9 @@ void __init dynamic_hugetlb_init(void)
 
 static int __init dynamic_hugetlb_setup(char *s)
 {
+	if (!s)
+		return 0;
+
 	if (!strcmp(s, "on"))
 		enable_dhugetlb = true;
 	return 1;
