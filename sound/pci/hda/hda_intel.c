0277bbae7f48b6054889a1fffd8cc44396f74d20
Juxin Gao <gaojuxin@loongson.cn>
2024-06-07 09:56:51 +0800

    ALSA: hda: Add Loongson LS7A HD-Audio support
    mainline inclusion
    from mainline-6.4-rc5
    commit 28bd137a3c8e105587ba8c55b68ef43b519b270f
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/IA45SK
    
    --------------------------------
    
    Add the new PCI ID 0x0014 0x7a07 and the new PCI ID 0x0014 0x7a37
    Loongson HDA controller.
    
    Signed-off-by: Yanteng Si <siyanteng@loongson.cn>
    Acked-by: Huacai Chen <chenhuacai@loongson.cn>
    Link: https://lore.kernel.org/r/993587483b9509796b29a416f257fcfb4b15c6ea.1686128807.git.siyanteng@loongson.cn
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Juxin Gao <gaojuxin@loongson.cn>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -276,6 +276,7 @@ enum {
 	AZX_DRIVER_ZHAOXIN,
 	AZX_DRIVER_ZXHDMI,
 	AZX_DRIVER_HYGON,
+	AZX_DRIVER_LOONGSON,
 	AZX_DRIVER_GENERIC,
 	AZX_NUM_DRIVERS, /* keep this as last entry */
 };
@@ -400,6 +401,7 @@ static const char * const driver_short_names[] = {
 	[AZX_DRIVER_ZHAOXIN] = "HDA Zhaoxin",
 	[AZX_DRIVER_ZXHDMI] = "HDA Zhaoxin GFX",
 	[AZX_DRIVER_HYGON] = "HDA Hygon",
+	[AZX_DRIVER_LOONGSON] = "HDA Loongson",
 	[AZX_DRIVER_GENERIC] = "HD-Audio Generic",
 };
 
@@ -2889,6 +2891,11 @@ static const struct pci_device_id azx_ids[] = {
 	/* Hygon HDAudio */
 	{ PCI_DEVICE(0x1d94, 0x14a9),
 	  .driver_data = AZX_DRIVER_HYGON | AZX_DCAPS_POSFIX_LPIB | AZX_DCAPS_NO_MSI },
+	/* Loongson HDAudio*/
+	{PCI_DEVICE(PCI_VENDOR_ID_LOONGSON, PCI_DEVICE_ID_LOONGSON_HDA),
+	  .driver_data = AZX_DRIVER_LOONGSON },
+	{PCI_DEVICE(PCI_VENDOR_ID_LOONGSON, PCI_DEVICE_ID_LOONGSON_HDMI),
+	  .driver_data = AZX_DRIVER_LOONGSON },
 	{ 0, }
 };
 MODULE_DEVICE_TABLE(pci, azx_ids);
