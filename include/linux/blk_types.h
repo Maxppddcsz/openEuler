7feb3eddb93de1735d4ee303ba449835cc178cdb
Yu Kuai <yukuai3@huawei.com>
2024-06-13 09:59:57 +0800

    block: support to dispatch bio asynchronously
    In certain environments, specific CPUs handle a large number of tasks
    and become bottlenecks, affecting overall system performance. This
    commit introduces a new feature that enables asynchronous I/O dispatch
    to designated CPUs, thereby relieving the pressure on the busy CPUs.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>
    Signed-off-by: Li Nan <linan122@huawei.com>

--- include/linux/blk_types.h
+++ include/linux/blk_types.h
@@ -301,6 +301,7 @@ enum {
 				 * of this bio. */
 	BIO_CGROUP_ACCT,	/* has been accounted to a cgroup */
 	BIO_TRACKED,		/* set if bio goes through the rq_qos path */
+	BIO_ASYNC,
 	BIO_FLAG_LAST
 };
 
