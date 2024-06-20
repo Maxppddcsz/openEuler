openeuler-mirror/kernel

commit 0d9fbb7dcca508b28683db7836f9f30fed1ec92a

crypto: jitter - change module_init(jent_mod_init) to subsys_initcall(jent_mod_init)
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/I9K8D1

--------------------------------

The ecdh-nist-p256 algorithm will depend on jitterentropy_rng,
and when they are build into kernel, the order of registration
should be done such that the underlying algorithms are ready
before the ones on top are registered.

We can enable fips=1 and ecdh, the calltrace like below:

alg: ecdh-nist-p256: test failed on vector 2, err=-14
Kernel panic - not syncing: alg: self-tests for ecdh-generic (ecdh)
failed in fips mode!
Call Trace:
 dump_stack+0x57/0x6e
 panic+0x109/0x2ca
 alg_test+0x414/0x420
 ? __switch_to_asm+0x3a/0x60
 ? __switch_to_asm+0x34/0x60
 ? __schedule+0x263/0x640
 ? crypto_acomp_scomp_free_ctx+0x30/0x30
 cryptomgr_test+0x22/0x40
 kthread+0xf9/0x130
 ? kthread_park+0x90/0x90
 ret_from_fork+0x22/0x30

The module_init(jent_mod_init) is later than subsys_initcall(ecdh_init),
so changing module_init(jent_mod_init) to subsys_initcall(jent_mod_init)
to fix it.

Fixes: c4741b230597 ("crypto: run initcalls for generic implementations earlier")
Signed-off-by: Gaosheng Cui <cuigaosheng1@huawei.com>

crypto/jitterentropy-kcapi.c
@@ -214,7 +214,7 @@ static void __exit jent_mod_exit(void)
 	crypto_unregister_rng(&jent_alg);
 }
 
-module_init(jent_mod_init);
+subsys_initcall(jent_mod_init);
 module_exit(jent_mod_exit);
 
 MODULE_LICENSE("Dual BSD/GPL");

Maxppddcsz/openEuler

commit 4cb75d2e48a5234aa60e3f389f0915238063e039

crypto: jitter - change subsys_initcall(jent_mod_init) back to module_init(jent_mod_init)
hulk inclusion
category: bugfix
bugzilla: https://gitee.com/openeuler/kernel/issues/IA6GL4

--------------------------------

jent_entropy_init depend on the clocksource being initialized, with the
subsys_initcall(jent_mod_init) called in arm32, the initialization of
clocksource is after jent_mod_init, so change the subsys_initcall in
arm32 to module_init. And the fips depend on jitter init, this feature
is controlled by CONFIG_CRYPTO_FIPS, so use CONFIG_CRYPTO_FIPS to isolate
the case of the fips problem for arm64/x86 with the dependance of
clocksource for arm32.

Fixes: 0d9fbb7dcca5 ("crypto: jitter - change module_init(jent_mod_init) to subsys_initcall(jent_mod_init)")
Signed-off-by: Xiang Yang <xiangyang3@huawei.com>

crypto/jitterentropy-kcapi.c
@@ -214,7 +214,11 @@ static void __exit jent_mod_exit(void)
 	crypto_unregister_rng(&jent_alg);
 }
 
+#if defined(CONFIG_CRYPTO_FIPS) && (defined(CONFIG_ARM64) || defined(CONFIG_X86))
 subsys_initcall(jent_mod_init);
+#else
+module_init(jent_mod_init);
+#endif
 module_exit(jent_mod_exit);
 
 MODULE_LICENSE("Dual BSD/GPL");