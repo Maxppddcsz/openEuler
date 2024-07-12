openeuler-mirror/kernel

commit 51d2e3963a25ced54305a1b680bf827d5ff0783a

x86/resctrl: Free the ctrlval arrays when domain_setup_mon_state() fails
stable inclusion
from stable-5.10.75
commit e4f7171c2395fd822585e721c25e1039759ed11d
bugzilla: 182987 https://gitee.com/openeuler/kernel/issues/I4I3MP

Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=e4f7171c2395fd822585e721c25e1039759ed11d

--------------------------------

commit 64e87d4bd3201bf8a4685083ee4daf5c0d001452 upstream.

domain_add_cpu() is called whenever a CPU is brought online. The
earlier call to domain_setup_ctrlval() allocates the control value
arrays.

If domain_setup_mon_state() fails, the control value arrays are not
freed.

Add the missing kfree() calls.

Fixes: 1bd2a63b4f0de ("x86/intel_rdt/mba_sc: Add initialization support")
Fixes: edf6fa1c4a951 ("x86/intel_rdt/cqm: Add RMID (Resource monitoring ID) management")
Signed-off-by: James Morse <james.morse@arm.com>
Signed-off-by: Borislav Petkov <bp@suse.de>
Acked-by: Reinette Chatre <reinette.chatre@intel.com>
Cc: <stable@vger.kernel.org>
Link: https://lkml.kernel.org/r/20210917165958.28313-1-james.morse@arm.com
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Signed-off-by: Chen Jun <chenjun102@huawei.com>
Acked-by: Weilong Chen <chenweilong@huawei.com>

Signed-off-by: Chen Jun <chenjun102@huawei.com>
Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>

arch/x86/kernel/cpu/resctrl/core.c
@@ -590,6 +590,8 @@ static void domain_add_cpu(int cpu, struct rdt_resource *r)
 	}
 
 	if (r->mon_capable && domain_setup_mon_state(r, d)) {
+		kfree(d->ctrl_val);
+		kfree(d->mbps_val);
 		kfree(d);
 		return;
 	}

Maxppddcsz/openEuler

commit 19c2e930d48c11f86efb8b95e66f9e55b07ba0a1

x86/resctrl: Free the ctrlval arrays in rdt_hw_domain
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I9U2JC
CVE: NA

--------------------------------

Fixes: 51d2e3963a25c ("x86/resctrl: Free the ctrlval arrays when domain_setup_mon_state() fails")

Signed-off-by: Wenkuan Wang <Wenkuan.Wang@amd.com>

arch/x86/kernel/cpu/resctrl/core.c
@@ -647,8 +647,8 @@ static void domain_add_cpu(int cpu, struct rdt_resource *r)
 	}
 
 	if (r->mon_capable && domain_setup_mon_state(r, d)) {
-		kfree(d->ctrl_val);
-		kfree(d->mbps_val);
+		kfree(hw_dom->ctrl_val);
+		kfree(hw_dom->mbps_val);
 		kfree(d);
 		return;
 	}